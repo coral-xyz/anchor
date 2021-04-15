f5f18c55af16c6542fbb9b0c3cbac5a532c8006e
name <nonsense>
2021-04-15 11:23:37 +0000

    added js mintTo function

--- examples/ido_pool/tests/ido_pool.js
+++ examples/ido_pool/tests/ido_pool.js
@@ -1,14 +1,154 @@
 const anchor = require('@project-serum/anchor');
+const assert = require('assert');
+
 
 describe('ido_pool', () => {
 
+  const provider = anchor.Provider.local();
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.Provider.env());
+  anchor.setProvider(provider);
 
-  it('Is initialized!', async () => {
-    // Add your test here.
-    const program = anchor.workspace.IdoPool;
-    const tx = await program.rpc.initialize();
-    console.log("Your transaction signature", tx);
+  const program = anchor.workspace.IdoPool;
+
+  let usdc_mint = null;
+  let watermelon_mint = null;
+
+
+  it('Initialize test state', async () => {
+    usdc_mint = await createMint(provider);
+    watermelon_mint = await createMint(provider);
+    creators_watermelon_publickey =  await createTokenAccount(provider, watermelon_mint, provider.wallet.publicKey);
+
+    await mintToAccount(provider, watermelon_mint, creators_watermelon_publickey, new anchor.BN(1000))
+
+    console.log(Object.getOwnPropertyNames(TokenInstructions).filter(function (p) {
+      return typeof TokenInstructions[p] === 'function';
+    }));
+
+    creators_watermelon_account = await getTokenAccount(provider, creators_watermelon_publickey)
+    console.log(creators_watermelon_account.amount)
+    assert.ok(creators_watermelon_account.amount.eq(new anchor.BN(1000)))
+    // const tx = await program.rpc.initialize();
+    // console.log('Your transaction signature', tx);
   });
 });
+
+
+
+
+// SPL token client boilerplate for test initialization. Everything below here is
+// mostly irrelevant to the point of the example.
+
+const serumCmn = require('@project-serum/common');
+const TokenInstructions = require('@project-serum/serum').TokenInstructions;
+
+// TODO: remove this constant once @project-serum/serum uses the same version
+//       of @solana/web3.js as anchor (or switch packages).
+const TOKEN_PROGRAM_ID = new anchor.web3.PublicKey(
+  TokenInstructions.TOKEN_PROGRAM_ID.toString()
+);
+
+async function getTokenAccount(provider, addr) {
+  return await serumCmn.getTokenAccount(provider, addr);
+}
+
+async function createMint(provider, authority) {
+  if (authority === undefined) {
+    authority = provider.wallet.publicKey;
+  }
+  const mint = new anchor.web3.Account();
+  const instructions = await createMintInstructions(
+    provider,
+    authority,
+    mint.publicKey
+  );
+
+  const tx = new anchor.web3.Transaction();
+  tx.add(...instructions);
+
+  await provider.send(tx, [mint]);
+
+  return mint.publicKey;
+}
+
+async function createMintInstructions(provider, authority, mint) {
+  let instructions = [
+    anchor.web3.SystemProgram.createAccount({
+      fromPubkey: provider.wallet.publicKey,
+      newAccountPubkey: mint,
+      space: 82,
+      lamports: await provider.connection.getMinimumBalanceForRentExemption(82),
+      programId: TOKEN_PROGRAM_ID,
+    }),
+    TokenInstructions.initializeMint({
+      mint,
+      decimals: 0,
+      mintAuthority: authority,
+    }),
+  ];
+  return instructions;
+}
+
+async function createTokenAccount(provider, mint, owner) {
+  const vault = new anchor.web3.Account();
+  const tx = new anchor.web3.Transaction();
+  tx.add(
+    ...(await createTokenAccountInstrs(provider, vault.publicKey, mint, owner))
+  );
+  await provider.send(tx, [vault]);
+  return vault.publicKey;
+}
+
+async function createTokenAccountInstrs(
+  provider,
+  newAccountPubkey,
+  mint,
+  owner,
+  lamports
+) {
+  if (lamports === undefined) {
+    lamports = await provider.connection.getMinimumBalanceForRentExemption(165);
+  }
+  return [
+    anchor.web3.SystemProgram.createAccount({
+      fromPubkey: provider.wallet.publicKey,
+      newAccountPubkey,
+      space: 165,
+      lamports,
+      programId: TOKEN_PROGRAM_ID,
+    }),
+    TokenInstructions.initializeAccount({
+      account: newAccountPubkey,
+      mint,
+      owner,
+    }),
+  ];
+}
+
+
+async function mintToAccount(provider, mint, destination, amount) {
+  // mint authority is the provider
+  const tx = new anchor.web3.Transaction();
+  tx.add(
+    ...(await createMintToAccountInstrs(mint, destination, amount, provider.wallet.publicKey))
+  );
+  // await provider.send(tx, [provider.wallet]);
+  await provider.send(tx, []);
+  return;
+}
+
+async function createMintToAccountInstrs(
+  mint,
+  destination,
+  amount,
+  mintAuthority
+) {
+  return [
+    TokenInstructions.mintTo({
+      mint,
+      destination: destination,
+      amount: amount,
+      mintAuthority: mintAuthority,
+    }),
+  ];
+}
