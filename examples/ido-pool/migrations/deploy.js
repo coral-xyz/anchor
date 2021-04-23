9658202733e9a53082c19972f4d2bdc877177018
armaniferrante <armaniferrante@gmail.com>
2021-04-23 11:08:42 -0700

    Remove newline in migrations file

--- examples/ido-pool/migrations/deploy.js
+++ examples/ido-pool/migrations/deploy.js
@@ -1,4 +1,3 @@
-
 // Migrations are an early feature. Currently, they're nothing more than this
 // single deploy script that's invoked from the CLI, injecting a provider
 // configured from the workspace's Anchor.toml.
