925c8228ce62aa62ea090001baba483a7da99575
armaniferrante <armaniferrante@gmail.com>
2021-05-05 14:52:12 -0700

    Remove rent

--- spl/src/dex.rs
+++ spl/src/dex.rs
@@ -166,5 +166,4 @@ pub struct CloseOpenOrders<'info> {
     pub authority: AccountInfo<'info>,
     pub destination: AccountInfo<'info>,
     pub market: AccountInfo<'info>,
-    pub rent: AccountInfo<'info>,
 }
