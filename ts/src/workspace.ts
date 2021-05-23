e800508f6474969f4111334729743d890dd3e384
armaniferrante <armaniferrante@gmail.com>
2021-05-23 11:39:13 -0700

    ts, cli: Overrideable workspace program addresses

--- ts/src/workspace.ts
+++ ts/src/workspace.ts
@@ -1,6 +1,8 @@
 import camelCase from "camelcase";
+import * as toml from "toml";
 import { PublicKey } from "@solana/web3.js";
 import { Program } from "./program";
+import { getProvider } from "./";
 
 let _populatedWorkspace = false;
 
@@ -44,12 +46,23 @@ const workspace = new Proxy({} as any, {
           const idlStr = fs.readFileSync(path);
           const idl = JSON.parse(idlStr);
           const name = camelCase(idl.name, { pascalCase: true });
-          programs[name] = new Program(
-            idl,
-            new PublicKey(idl.metadata.address)
-          );
+          if (idl.metadata && idl.metadata.address) {
+            programs[name] = new Program(
+              idl,
+              new PublicKey(idl.metadata.address)
+            );
+          }
           return programs;
         }, workspaceCache);
+
+      // Override the workspace programs if the user put them in the config.
+      const anchorToml = toml.parse(
+        fs.readFileSync(path.join(projectRoot, "Anchor.toml"), "utf-8")
+      );
+      const clusterId = anchorToml.provider.cluster;
+      if (anchorToml.clusters && anchorToml.clusters[clusterId]) {
+        attachWorkspaceOverride(workspaceCache, anchorToml.clusters[clusterId]);
+      }
 
       _populatedWorkspace = true;
     }
@@ -57,5 +70,20 @@ const workspace = new Proxy({} as any, {
     return workspaceCache[programName];
   },
 });
+
+function attachWorkspaceOverride(
+  workspaceCache: { [key: string]: Program },
+  overrideConfig: { [key: string]: string }
+) {
+  Object.keys(overrideConfig).forEach((programName) => {
+    const wsProgramName = camelCase(programName, { pascalCase: true });
+    const oldProgram = workspaceCache[wsProgramName];
+    const overrideAddress = new PublicKey(overrideConfig[programName]);
+    workspaceCache[wsProgramName] = new Program(
+      oldProgram.idl,
+      overrideAddress
+    );
+  });
+}
 
 export default workspace;
