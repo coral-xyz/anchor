ed805c829063ecd704c0d3e51c8737567623db9f
Standaa <stanislasdrg@gmail.com>
2021-06-16 17:23:51 +0200

    fix: Wrong assumption that program logs always belong to last program context

--- ts/src/program/event.ts
+++ ts/src/program/event.ts
@@ -44,8 +44,6 @@ export class EventParser {
       }
       if (didPop) {
         execution.pop();
-        // Skip the "success" log, which always follows the consumed log.
-        logScanner.next();
       }
       log = logScanner.next();
     }
@@ -89,18 +87,20 @@ export class EventParser {
   private handleSystemLog(log: string): [string | null, boolean] {
     // System component.
     const logStart = log.split(":")[0];
-    // Recursive call.
-    if (logStart.startsWith(`Program ${this.programId.toString()} invoke`)) {
+    // Did the program finish executing?
+    if (
+      logStart.includes("success") ||
+      logStart.match(/^Program (.*) consumed .*$/g) !== null
+    ) {
+      return [null, true];
+      // Recursive call.
+    } else if (
+      logStart.startsWith(`Program ${this.programId.toString()} invoke`)
+    ) {
       return [this.programId.toString(), false];
-    }
-    // Cpi call.
-    else if (logStart.includes("invoke")) {
+    } else if (logStart.includes("invoke")) {
       return ["cpi", false]; // Any string will do.
     } else {
-      // Did the program finish executing?
-      if (logStart.match(/^Program (.*) consumed .*$/g) !== null) {
-        return [null, true];
-      }
       return [null, false];
     }
   }
