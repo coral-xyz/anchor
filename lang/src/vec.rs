53b7ee4ae4e3e73a3e96037f9a5b58329da0ca8d
CanardMandarin <thibault.marboud@gmail.com>
2021-06-25 00:17:28 +0200

    lang: add test Accounts trait for Vec

--- lang/src/vec.rs
+++ lang/src/vec.rs
@@ -3,7 +3,6 @@ use solana_program::account_info::AccountInfo;
 use solana_program::instruction::AccountMeta;
 use solana_program::program_error::ProgramError;
 use solana_program::pubkey::Pubkey;
-use std::iter::FromIterator;
 
 impl<'info, T: ToAccountInfos<'info>> ToAccountInfos<'info> for Vec<T> {
     fn to_account_infos(&self) -> Vec<AccountInfo<'info>> {
@@ -27,9 +26,9 @@ impl<'info, T: Accounts<'info>> Accounts<'info> for Vec<T> {
         accounts: &mut &[AccountInfo<'info>],
         ix_data: &[u8],
     ) -> Result<Self, ProgramError> {
-        Ok(Vec::from_iter(T::try_accounts(
-            program_id, accounts, ix_data,
-        )))
+        let mut vec: Vec<T> = Vec::new();
+        T::try_accounts(program_id, accounts, ix_data).map(|item| vec.push(item))?;
+        Ok(vec)
     }
 }
 
@@ -52,21 +51,45 @@ mod tests {
         let program_id = Pubkey::default();
 
         let key = Pubkey::default();
-        let mut lamports = 0;
-        let mut data = vec![0; 10];
+        let mut lamports1 = 0;
+        let mut data1 = vec![0; 10];
         let owner = Pubkey::default();
-        let account = AccountInfo::new(
+        let account1 = AccountInfo::new(
             &key,
             true,
             true,
-            &mut lamports,
-            &mut data,
+            &mut lamports1,
+            &mut data1,
             &owner,
             false,
             Epoch::default(),
         );
 
-        let mut accounts = &[account][..];
+        let mut lamports2 = 0;
+        let mut data2 = vec![0; 10];
+        let account2 = AccountInfo::new(
+            &key,
+            true,
+            true,
+            &mut lamports2,
+            &mut data2,
+            &owner,
+            false,
+            Epoch::default(),
+        );
+
+        let mut accounts = &[account1, account2][..];
+        let parsed_accounts = Vec::<Test>::try_accounts(&program_id, &mut accounts, &[]).unwrap();
+
+        assert_eq!(accounts.len(), parsed_accounts.len());
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_accounts_trait_for_vec_empty() {
+        let program_id = Pubkey::default();
+
+        let mut accounts = &[][..];
         Vec::<Test>::try_accounts(&program_id, &mut accounts, &[]).unwrap();
     }
 }
