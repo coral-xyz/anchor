bd5f8158182619a1287fc84f381e398a352d3627
CanardMandarin <thibault.marboud@gmail.com>
2021-06-24 21:08:40 +0200

    lang: add Accounts trait for Vec

--- lang/src/vec.rs
+++ lang/src/vec.rs
@@ -1,6 +1,9 @@
-use crate::{ToAccountInfos, ToAccountMetas};
+use crate::{Accounts, ToAccountInfos, ToAccountMetas};
 use solana_program::account_info::AccountInfo;
 use solana_program::instruction::AccountMeta;
+use solana_program::program_error::ProgramError;
+use solana_program::pubkey::Pubkey;
+use std::iter::FromIterator;
 
 impl<'info, T: ToAccountInfos<'info>> ToAccountInfos<'info> for Vec<T> {
     fn to_account_infos(&self) -> Vec<AccountInfo<'info>> {
@@ -15,5 +18,17 @@ impl<T: ToAccountMetas> ToAccountMetas for Vec<T> {
         self.iter()
             .flat_map(|item| (*item).to_account_metas(is_signer))
             .collect()
+    }
+}
+
+impl<'info, T: Accounts<'info>> Accounts<'info> for Vec<T> {
+    fn try_accounts(
+        program_id: &Pubkey,
+        accounts: &mut &[AccountInfo<'info>],
+        ix_data: &[u8],
+    ) -> Result<Self, ProgramError> {
+        Ok(Vec::from_iter(T::try_accounts(
+            program_id, accounts, ix_data,
+        )))
     }
 }
