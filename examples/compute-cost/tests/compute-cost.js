f5803a71c4f343aeea6552eb5a8810ea6b72a2a4
name <nonsense>
2021-06-25 18:23:02 +0000

    pda signed cpi tests

--- examples/compute-cost/tests/compute-cost.js
+++ examples/compute-cost/tests/compute-cost.js
@@ -17,24 +17,67 @@ describe('compute-cost', () => {
     // Add your test here.
     const program = anchor.workspace.ComputeCost;
     let provider = program.provider;
+    
+    let [pdaSigner, nonce] = await anchor.web3.PublicKey.findProgramAddress(
+      [provider.wallet.publicKey.toBuffer()],
+      program.programId
+    );
 
     usdcMint = await utils.createMint(program.provider, provider.wallet.publicKey, decimals);
     // Give the provider account a bunch of USDC to play around with
     fromUsdc = await utils.createTokenAccount(provider, usdcMint, provider.wallet.publicKey);
     await utils.mintToAccount(provider, usdcMint, fromUsdc, new anchor.BN(1000000000000000), provider.wallet.publicKey);
-    toUsdc = await utils.createTokenAccount(provider, usdcMint, provider.wallet.publicKey);
+    toUsdc = await utils.createTokenAccount(provider, usdcMint, pdaSigner);
+    await utils.mintToAccount(provider, usdcMint, toUsdc, new anchor.BN(1000000000000000), provider.wallet.publicKey);
+
 
     // Transfer over a dollar at time
     const oneDollar = new anchor.BN(1000000);
-    const numTransfers = new anchor.BN(20);
-    const tx = await program.rpc.transferPlease(numTransfers, {
+
+    for (let i = 27; i < 28; i++) {
+      let numTransfers = new anchor.BN(i);
+      console.log("Num transfers: ", numTransfers);
+      let tx = await program.rpc.transferPlease(numTransfers, {
+        accounts: {
+          fromUsdc,
+          toUsdc,
+          authority: provider.wallet.publicKey,
+          tokenProgram: TOKEN_PROGRAM_ID
+        }
+      });
+    };
+
+    await program.rpc.initSigner(nonce, {
       accounts: {
-        fromUsdc,
-        toUsdc,
+        pdaSigner,
         authority: provider.wallet.publicKey,
-        tokenProgram: TOKEN_PROGRAM_ID
+        systemProgram: anchor.web3.SystemProgram.programId,
+        rent: anchor.web3.SYSVAR_RENT_PUBKEY,
       }
     });
-    console.log("Your transaction signature", tx);
+    
+    let pdaSignerData = await program.account.pdaSigner.fetch(
+      pdaSigner
+    );
+    console.log(pdaSignerData);
+
+
+    for (let i = 27; i < 28; i++) {
+      let numTransfers = new anchor.BN(i);
+      console.log("Num transfers: ", numTransfers);
+      await program.rpc.signedTransfer(numTransfers, {
+        accounts: {
+          pdaSigner,
+          fromUsdc,
+          toUsdc,
+          authority: provider.wallet.publicKey,
+          tokenProgram: TOKEN_PROGRAM_ID
+        }
+      });
+    };
+
+
+
+    // console.log("Your transaction signature", tx);
   });
 });
