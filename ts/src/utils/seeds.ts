9c0ee156319b23809a9e6c238f4b4a01eee69cb0
Radoslaw Domanski <woohaas@gmail.com>
2021-08-12 15:34:36 +1200

    easy seeds generation
    - added a new util that makes code simpler/easier to read
    - added a test for it

---
+++ ts/src/utils/seeds.ts
@@ -0,0 +1,24 @@
+import {utils} from "../index";
+import {PublicKey} from "@solana/web3.js";
+
+/**
+ * can be used like that:
+ *     const [pda, bump] = await PublicKey.findProgramAddress(
+ *       Seeds.from(signer.publicKey, "input"),
+ *       this.program.programId
+ *     )
+ * or like that:
+ *     const [pda, bump] = await PublicKey.findProgramAddress(
+ *       [signer.publicKey.toBuffer(), ...Seeds.from("input")],
+ *       this.program.programId
+ *     )
+ */
+export function from(...stringSeeds: (string | PublicKey)[]): Buffer[] {
+  return stringSeeds.map(it => {
+    if (typeof it === 'string') {
+      return Buffer.from(utils.bytes.utf8.encode(it));
+    } else {
+      return it.toBuffer();
+    }
+  })
+}
