405a4cf791ee00a1fe741be544ec7625e378c941
Tom Linton <tlinton@fastmail.fm>
2021-10-04 15:38:03 +1300

    Revert "Remove extra file"
    This reverts commit 88d727e7f4e7aa3260d5ce854afd420fd19a5bcd.

---
+++ examples/tutorial/basic-0/client.js
@@ -0,0 +1,27 @@
+// client.js is used to introduce the reader to generating clients from IDLs.
+// It is not expected users directly test with this example. For a more
+// ergonomic example, see `tests/basic-0.js` in this workspace.
+
+const anchor = require('@project-serum/anchor');
+
+// Configure the local cluster.
+anchor.setProvider(anchor.Provider.local());
+
+async function main() {
+  // #region main
+  // Read the generated IDL.
+  const idl = JSON.parse(require('fs').readFileSync('./target/idl/basic_0.json', 'utf8'));
+
+  // Address of the deployed program.
+  const programId = new anchor.web3.PublicKey('<YOUR-PROGRAM-ID>');
+
+  // Generate the program client from IDL.
+  const program = new anchor.Program(idl, programId);
+
+  // Execute the RPC.
+  await program.rpc.initialize();
+  // #endregion main
+}
+
+console.log('Running client.');
+main().then(() => console.log('Success'));
