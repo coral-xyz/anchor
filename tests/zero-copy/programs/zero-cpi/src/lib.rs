4ff3a1d3ded9829d72e4ac7a95cd663ec72b28a7
Norbert Bodziony <norbertbodziony@gmail.com>
2021-10-08 11:52:27 +0200

    add zero_copy cpi to tests

---
+++ tests/zero-copy/programs/zero-cpi/src/lib.rs
@@ -0,0 +1,36 @@
+use anchor_lang::prelude::*;
+use zero_copy::program::ZeroCopy;
+use zero_copy::{self, Bar, Foo, UpdateBar};
+
+declare_id!("ErjUjtqKE5AGWUsjseSJCVLtddM6rhaMbDqmhzraF9h6");
+
+#[program]
+pub mod zero_cpi {
+    use super::*;
+    pub fn check_cpi(ctx: Context<CheckCpi>, data: u64) -> ProgramResult {
+        // AccountLoader::to_account_info(&self)
+        let cpi_program = ctx.accounts.zero_copy_program.to_account_info();
+        let cpi_accounts = UpdateBar {
+            authority: ctx.accounts.authority.clone(),
+            bar: ctx.accounts.bar.clone(),
+            foo: ctx.accounts.foo.clone(),
+        };
+        let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
+        // zero_copy::cpi::(cpi_ctx, data)
+        zero_copy::cpi::update_bar(cpi_ctx, data);
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct CheckCpi<'info> {
+    #[account(
+        mut,
+        has_one = authority,
+    )]
+    bar: AccountLoader<'info, Bar>,
+    #[account(signer)]
+    authority: AccountInfo<'info>,
+    foo: AccountLoader<'info, Foo>,
+    zero_copy_program: Program<'info, ZeroCopy>,
+}
