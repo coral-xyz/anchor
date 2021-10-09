24d07aa41b0b4e368f1467e315ca0357a7e3febb
NorbertBodziony <norbertbodziony@gmail.com>
2021-10-09 23:14:14 +0200

    ts: Types fixes (#850)

--- ts/types/buffer-layout/index.d.ts
+++ ts/types/buffer-layout/index.d.ts
@@ -7,7 +7,7 @@ declare module 'buffer-layout' {
 
     constructor(span: number, property?: string);
 
-    decode(b: Buffer, offset?: number): T;
+    decode(b: Buffer | string, offset?: number): T;
     encode(src: T, b: Buffer, offset?: number): number;
     getSpan(b: Buffer, offset?: number): number;
     replicate(name: string): this;
