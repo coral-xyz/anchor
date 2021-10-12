587f99737a1b19bf1f1326d64175e02beec5e96f
Alan O'Donnell <alan.m.odonnell@gmail.com>
2021-10-12 00:01:00 -0700

    More robust + nicer error messages.

--- lang/syn/src/codegen/program/program_id.rs
+++ lang/syn/src/codegen/program/program_id.rs
@@ -3,15 +3,50 @@ use quote::quote;
 use crate::Program;
 
 pub fn generate(program: &Program) -> proc_macro2::TokenStream {
-    let anchor_toml = std::fs::read_to_string("./Anchor.toml")
-        .map(|s| s.parse::<toml::Value>())
-        .unwrap()
-        .unwrap();
-    let cluster = std::env::var("ANCHOR_CLUSTER").unwrap_or("localnet".to_string());
-    let program_id = anchor_toml["programs"][cluster][&program.name.to_string()].to_string();
-    // strip quotes, urgh
-    let program_id = &program_id[1..program_id.len() - 1];
+    let program_name = &program.name.to_string();
+    let program_id = program_id_from_anchor_toml(program_name)
+        .or_else(|| program_id_from_target_slash_deploy_dir(program_name))
+        .unwrap_or_else(|| {
+            panic!("Expected to find {}'s program_id in your Anchor.toml file's program section or in ./target/deploy/{}-keypair.json", program_name, program_name);
+        });
     quote! {
         ::anchor_lang::declare_id!(#program_id);
     }
+}
+
+fn program_id_from_anchor_toml(program_name: &str) -> Option<String> {
+    let anchor_toml = std::fs::read_to_string("./Anchor.toml")
+        .expect("Expected to find an Anchor.toml file in the root of your project.")
+        .parse::<toml::Value>()
+        .expect("Expected your Anchor.toml file to be valid TOML.");
+
+    let provider = anchor_toml.get("provider")?;
+    let cluster = provider
+        .get("cluster")?
+        .as_str()
+        .expect("Your Anchor.toml [provider]'s cluster field should be a string.");
+
+    let program_id = anchor_toml
+        .get("programs")?
+        .get(cluster)?
+        .get(program_name)?
+        .as_str()
+        .unwrap_or_else(|| {
+            panic!(
+                "Expected your Anchor.toml's [programs.{}] {} field to be a string.",
+                cluster, program_name
+            )
+        });
+
+    eprintln!("Got an anchor.toml program_id: {}", program_id);
+    Some(program_id.to_string())
+}
+
+fn program_id_from_target_slash_deploy_dir(program_name: &str) -> Option<String> {
+    let path = format!("./target/deploy/{}-keypair.json", program_name);
+    let bytes: Vec<u8> = serde_json::from_str(&std::fs::read_to_string(path).ok()?).ok()?;
+    let pubkey_bytes = &bytes[bytes.len() - 32..];
+    let pubkey = bs58::encode(pubkey_bytes).into_string();
+    eprintln!("Got a target/deploy pubkey: {}", pubkey);
+    Some(pubkey)
 }
