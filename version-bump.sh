a60d363c3db74cea203e6de86a714efdf962d3f9
Tom Linton <tlinton@fastmail.fm>
2021-11-12 13:49:48 +1300

    Insert line into CHANGELOG.md

--- version-bump.sh
+++ version-bump.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+[ $# -eq 1 ] || exit
+
 echo "Bumping versions to $1"
 
 git grep -l $(cat VERSION) ':!*.lock' ':!CHANGELOG.md' | xargs sed -i '' -e "s/$(cat VERSION)/$1/g"
+
+# Insert version number into CHANGELOG.md
+sed -i '' -e "s/## [Unreleased]/## [Unreleased]\n\n[$1] - $(date '+%Y-%m-%d')/" CHANGELOG.md
+
 echo $1 > VERSION
