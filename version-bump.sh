d23c28f54a3804649ada710a6b3984eb55aebb56
Tom Linton <tlinton@fastmail.fm>
2021-11-14 12:45:46 +1300

    Make it work for BSD or GNU sed

--- version-bump.sh
+++ version-bump.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
-[ $# -eq 1 ] || exit
+if [ $# -eq 0 ]; then
+    echo "Usage $0 VERSION"
+    exit 1
+fi
 
 echo "Bumping versions to $1"
 
-git grep -l $(cat VERSION) ':!*.lock' ':!CHANGELOG.md' | xargs sed -i '' -e "s/$(cat VERSION)/$1/g"
+# GNU/BSD compat
+sedi=(-i)
+case "$(uname)" in
+  # For macOS, use two parameters
+  Darwin*) sedi=(-i "")
+esac
+
+git grep -l $(cat VERSION) ':!*.lock' ':!CHANGELOG.md' | xargs sed "${sedi[@]}" -e "s/$(cat VERSION)/$1/g"
 
 # Insert version number into CHANGELOG.md
-sed -i '' -e "s/## [Unreleased]/## [Unreleased]\n\n[$1] - $(date '+%Y-%m-%d')/" CHANGELOG.md
+sed "${sedi[@]}" -e "s/## [Unreleased]/## [Unreleased]\n\n[$1] - $(date '+%Y-%m-%d')/" CHANGELOG.md
 
 echo $1 > VERSION
+
+echo "$(cat VERSION) changeset generated, commit and tag"
