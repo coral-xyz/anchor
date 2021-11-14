3c7d3b514b387f95485d95d994603be852b9be14
Tom Linton <tlinton@fastmail.fm>
2021-11-14 13:01:23 +1300

    Improved stats

--- version-bump.sh
+++ version-bump.sh
@@ -14,13 +14,13 @@ case "$(uname)" in
   Darwin*) sedi=(-i "")
 esac
 
-git grep -l $(cat VERSION) -- './*' ':!**/yarn.lock' ':!CHANGELOG.md' | xargs sed "${sedi[@]}" -e "s/$(cat VERSION)/$1/g" | wc
+git grep -l $(cat VERSION) -- './*' ':!**/yarn.lock' ':!CHANGELOG.md' | xargs sed "${sedi[@]}" -e "s/$(cat VERSION)/$1/g"
 
 # Insert version number into CHANGELOG.md
 sed "${sedi[@]}" -e "s/## [Unreleased]/## [Unreleased]\n\n[$1] - $(date '+%Y-%m-%d')/" CHANGELOG.md
 
 echo $1 > VERSION
 
-echo "$(git diff --numstat | wc -l) modified files"
+echo "$(git diff --stat | tail -n1) files modified"
 
-echo "$(cat VERSION) changeset generated, commit and tag"
+echo " $(cat VERSION) changeset generated, commit and tag"
