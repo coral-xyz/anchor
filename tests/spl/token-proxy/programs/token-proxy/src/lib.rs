4f2c1ce2a9b298d5802fb8ce47255de67db77216
Paul Schaaf <paulsimonschaaf@gmail.com>
2021-11-16 00:50:16 +0100

    tests: fix spl tests

--- tests/spl/token-proxy/programs/token-proxy/src/lib.rs
+++ tests/spl/token-proxy/programs/token-proxy/src/lib.rs
@@ -88,13 +88,11 @@ impl<'a, 'b, 'c, 'info> From<&mut ProxyTransfer<'info>>
     for CpiContext<'a, 'b, 'c, 'info, Transfer<'info>>
 {
     fn from(accounts: &mut ProxyTransfer<'info>) -> CpiContext<'a, 'b, 'c, 'info, Transfer<'info>> {
-        let cpi_accounts = Transfer {
+        CpiContext::new(Transfer {
             from: accounts.from.clone(),
             to: accounts.to.clone(),
             authority: accounts.authority.clone(),
-        };
-        let cpi_program = accounts.token_program.clone();
-        CpiContext::new(cpi_program, cpi_accounts)
+        })
     }
 }
 
@@ -102,25 +100,21 @@ impl<'a, 'b, 'c, 'info> From<&mut ProxyMintTo<'info>>
     for CpiContext<'a, 'b, 'c, 'info, MintTo<'info>>
 {
     fn from(accounts: &mut ProxyMintTo<'info>) -> CpiContext<'a, 'b, 'c, 'info, MintTo<'info>> {
-        let cpi_accounts = MintTo {
+        CpiContext::new(MintTo {
             mint: accounts.mint.clone(),
             to: accounts.to.clone(),
             authority: accounts.authority.clone(),
-        };
-        let cpi_program = accounts.token_program.clone();
-        CpiContext::new(cpi_program, cpi_accounts)
+        })
     }
 }
 
 impl<'a, 'b, 'c, 'info> From<&mut ProxyBurn<'info>> for CpiContext<'a, 'b, 'c, 'info, Burn<'info>> {
     fn from(accounts: &mut ProxyBurn<'info>) -> CpiContext<'a, 'b, 'c, 'info, Burn<'info>> {
-        let cpi_accounts = Burn {
+        CpiContext::new(Burn {
             mint: accounts.mint.clone(),
             to: accounts.to.clone(),
             authority: accounts.authority.clone(),
-        };
-        let cpi_program = accounts.token_program.clone();
-        CpiContext::new(cpi_program, cpi_accounts)
+        })
     }
 }
 
@@ -130,12 +124,11 @@ impl<'a, 'b, 'c, 'info> From<&mut ProxySetAuthority<'info>>
     fn from(
         accounts: &mut ProxySetAuthority<'info>,
     ) -> CpiContext<'a, 'b, 'c, 'info, SetAuthority<'info>> {
-        let cpi_accounts = SetAuthority {
+        // TODO: Support multisig signers
+        CpiContext::new(SetAuthority {
             account_or_mint: accounts.account_or_mint.clone(),
             current_authority: accounts.current_authority.clone(),
-        }; // TODO: Support multisig signers
-        let cpi_program = accounts.token_program.clone();
-        CpiContext::new(cpi_program, cpi_accounts)
+        })
     }
 }
 
