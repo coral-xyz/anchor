d27830aeb1e000a5b2987b62ff82580d4495f9a2
Armani Ferrante <armaniferrante@gmail.com>
2022-01-08 23:25:29 -0500

    specify coder iface

--- ts/src/coder/spl-token/index.ts
+++ ts/src/coder/spl-token/index.ts
@@ -1,4 +1,5 @@
 import { Idl } from "../../idl.js";
+import { Coder } from "../index.js";
 import { SplTokenInstructionCoder } from "./instruction.js";
 import { SplTokenStateCoder } from "./state.js";
 import { SplTokenAccountsCoder } from "./accounts.js";
@@ -7,7 +8,7 @@ import { SplTokenEventsCoder } from "./events.js";
 /**
  * Coder for the SPL token program.
  */
-export class SplTokenCoder {
+export class SplTokenCoder implements Coder {
   readonly instruction: SplTokenInstructionCoder;
   readonly accounts: SplTokenAccountsCoder;
   readonly state: SplTokenStateCoder;
