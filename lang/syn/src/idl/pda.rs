0f8daba86cc421fe9e2d8ed89dacf6e986298587
Armani Ferrante <armaniferrante@gmail.com>
2022-01-24 09:09:07 -0500

    update

--- lang/syn/src/idl/pda.rs
+++ lang/syn/src/idl/pda.rs
@@ -39,7 +39,6 @@ pub fn parse(
     if !seeds_feature {
         return None;
     }
-
     let pda_parser = PdaParser::new(ctx, accounts);
     acc.constraints
         .seeds
@@ -67,6 +66,7 @@ impl<'a> PdaParser<'a> {
         let ix_args = accounts.instruction_args().unwrap_or_default();
         let const_names: Vec<String> = ctx.consts().map(|c| c.ident.to_string()).collect();
         let account_field_names = accounts.field_names();
+
         Self {
             ctx,
             accounts,
@@ -81,135 +81,161 @@ impl<'a> PdaParser<'a> {
         let seeds = seeds_grp
             .seeds
             .iter()
-            .map(|seed| {
-                parse_seed(
-                    self.ctx,
-                    self.accounts,
-                    &self.ix_args,
-                    &self.const_names,
-                    &self.account_field_names,
-                    seed,
-                )
-            })
+            .map(|s| self.parse_seed(s))
             .collect::<Option<Vec<_>>>()?;
 
         // Parse the program id from the constraints.
         let program_id = seeds_grp
             .program_seed
             .as_ref()
-            .map(|pid| {
-                parse_seed(
-                    self.ctx,
-                    self.accounts,
-                    &self.ix_args,
-                    &self.const_names,
-                    &self.account_field_names,
-                    pid,
-                )
-            })
+            .map(|pid| self.parse_seed(pid))
             .unwrap_or_default();
 
         // Done.
         Some(IdlPda { seeds, program_id })
     }
-}
 
-fn parse_seed(
-    ctx: &CrateContext,
-    accounts: &AccountsStruct,
-    ix_args: &HashMap<String, String>,
-    const_names: &[String],
-    account_field_names: &[String],
-    seed: &Expr,
-) -> Option<IdlSeed> {
-    match seed {
-        Expr::MethodCall(_) => {
-            // Parse the seed components.
-            let seed_path = parse_seed_path(seed)?;
+    fn parse_seed(&self, seed: &Expr) -> Option<IdlSeed> {
+        println!("EXPR: {:?}", parser::tts_to_string(seed));
+        match seed {
+            Expr::MethodCall(_) => {
+                let seed_path = parse_seed_path(seed)?;
 
-            match seed_path {
-                // Instruction argument.
-                _ if ix_args.contains_key(&seed_path.name()) => {
-                    let idl_ty = IdlType::from_str(ix_args.get(&seed_path.name()).unwrap()).ok()?;
-                    Some(IdlSeed::Arg(IdlSeedArg {
-                        ty: idl_ty,
-                        path: seed_path.path(),
-                    }))
-                }
-                // Constant.
-                _ if const_names.contains(&seed_path.name()) => {
-                    // Pull in the constant value directly into the IDL.
-                    assert!(seed_path.components().is_empty());
-                    let const_item = ctx.consts().find(|c| c.ident == seed_path.name()).unwrap();
-                    let idl_ty = IdlType::from_str(&parser::tts_to_string(&const_item.ty)).ok()?;
-                    let mut idl_ty_value = parser::tts_to_string(&const_item.expr);
-
-                    if let IdlType::Array(_ty, _size) = &idl_ty {
-                        if idl_ty_value.contains("b\"") {
-                            let components: Vec<&str> = idl_ty_value.split('b').collect();
-                            assert!(components.len() == 2);
-                            let mut str_lit = components[1].to_string();
-                            str_lit.retain(|c| c != '"');
-                            idl_ty_value = format!("{:?}", str_lit.as_bytes());
-                        }
-                    }
-
-                    Some(IdlSeed::Const(IdlSeedConst {
-                        ty: idl_ty,
-                        value: serde_json::from_str(&idl_ty_value).unwrap(),
-                    }))
-                }
-                // Account pubkey or account data.
-                _ if account_field_names.contains(&seed_path.name()) => {
-                    Some(IdlSeed::Account(IdlSeedAccount {
-                        ty: parse_seed_account_field_ty(
-                            ctx,
-                            accounts,
-                            seed_path.name().to_string(),
-                            seed_path.components(),
-                        )?,
-                        path: seed_path.path(),
-                        account: parse_seed_account_ty(ctx, accounts, seed_path.name()),
-                    }))
-                }
-                // String literal.
-                _ if seed_path.components().is_empty() && seed_path.name().contains('"') => {
-                    let mut var_name = seed_path.name().to_string();
-                    // Remove the byte `b` prefix if the string is of the form `b"seed".
-                    if var_name.starts_with("b\"") {
-                        var_name.remove(0);
-                    }
-                    let value_string: String = var_name.chars().filter(|c| *c != '"').collect();
-                    Some(IdlSeed::Const(IdlSeedConst {
-                        value: serde_json::Value::String(value_string),
-                        ty: IdlType::String,
-                    }))
-                }
-                // Unknown.
-                _ => {
+                if self.is_instruction(&seed_path) {
+                    self.parse_instruction(&seed_path)
+                } else if self.is_const(&seed_path) {
+                    self.parse_const(&seed_path)
+                } else if self.is_account(&seed_path) {
+                    self.parse_account(&seed_path)
+                } else if self.is_str_literal(&seed_path) {
+                    self.parse_str_literal(&seed_path)
+                } else {
                     println!("WARNING: unexpected seed category for var: {:?}", seed_path);
                     None
                 }
             }
+            Expr::Reference(expr_reference) => self.parse_seed(&expr_reference.expr),
+            Expr::Index(_) => {
+                println!("WARNING: auto pda derivation not currently supported for slice literals");
+                None
+            }
+            // Unknown type. Please file an issue	.
+            _ => {
+                println!("WARNING: unexpected seed: {:?}", seed);
+                None
+            }
         }
-        Expr::Reference(expr_reference) => parse_seed(
-            ctx,
-            accounts,
-            ix_args,
-            const_names,
-            account_field_names,
-            &expr_reference.expr,
-        ),
-        Expr::Index(_) => {
-            // Slice literal.
-            println!("WARNING: auto pda derivation not currently supported for slice literals");
-            None
+    }
+
+    fn parse_instruction(&self, seed_path: &SeedPath) -> Option<IdlSeed> {
+        let idl_ty = IdlType::from_str(self.ix_args.get(&seed_path.name()).unwrap()).ok()?;
+        Some(IdlSeed::Arg(IdlSeedArg {
+            ty: idl_ty,
+            path: seed_path.path(),
+        }))
+    }
+
+    fn parse_const(&self, seed_path: &SeedPath) -> Option<IdlSeed> {
+        // Pull in the constant value directly into the IDL.
+        assert!(seed_path.components().is_empty());
+        let const_item = self
+            .ctx
+            .consts()
+            .find(|c| c.ident == seed_path.name())
+            .unwrap();
+        let idl_ty = IdlType::from_str(&parser::tts_to_string(&const_item.ty)).ok()?;
+        let mut idl_ty_value = parser::tts_to_string(&const_item.expr);
+
+        if let IdlType::Array(_ty, _size) = &idl_ty {
+            if idl_ty_value.contains("b\"") {
+                let components: Vec<&str> = idl_ty_value.split('b').collect();
+                assert!(components.len() == 2);
+                let mut str_lit = components[1].to_string();
+                str_lit.retain(|c| c != '"');
+                idl_ty_value = format!("{:?}", str_lit.as_bytes());
+            }
         }
-        _ => {
-            // Unknown type. Please file an issue.
-            println!("WARNING: unexpected seed: {:?}", seed);
-            None
+
+        Some(IdlSeed::Const(IdlSeedConst {
+            ty: idl_ty,
+            value: serde_json::from_str(&idl_ty_value).unwrap(),
+        }))
+    }
+
+    fn parse_account(&self, seed_path: &SeedPath) -> Option<IdlSeed> {
+        // Get the anchor account field from the derive accounts struct.
+        let account_field = self
+            .accounts
+            .fields
+            .iter()
+            .find(|field| *field.ident() == seed_path.name())
+            .unwrap();
+
+        // Name of the account struct.
+        let account = account_field.ty_name()?;
+
+        // Follow the path to find the seed type.
+        let ty = {
+            let mut path = seed_path.components();
+            match path.len() {
+                0 => IdlType::PublicKey,
+                1 => {
+                    if account == "TokenAccount" {
+                        assert!(path.len() == 1);
+                        match path[0].as_str() {
+                            "mint" => IdlType::PublicKey,
+                            "amount" => IdlType::U64,
+                            "authority" => IdlType::PublicKey,
+                            "delegated_amount" => IdlType::U64,
+                            _ => {
+                                println!("WARNING: token field isn't supported: {}", &path[0]);
+                                return None;
+                            }
+                        }
+                    } else {
+                        // Get the rust representation of the field's struct.
+                        let strct = self.ctx.structs().find(|s| s.ident == account).unwrap();
+                        parse_field_path(self.ctx, strct, &mut path)
+                    }
+                }
+                _ => panic!("invariant violation"),
+            }
+        };
+
+        Some(IdlSeed::Account(IdlSeedAccount {
+            ty,
+            account: Some(account),
+            path: seed_path.path(),
+        }))
+    }
+
+    fn parse_str_literal(&self, seed_path: &SeedPath) -> Option<IdlSeed> {
+        let mut var_name = seed_path.name();
+        // Remove the byte `b` prefix if the string is of the form `b"seed".
+        if var_name.starts_with("b\"") {
+            var_name.remove(0);
         }
+        let value_string: String = var_name.chars().filter(|c| *c != '"').collect();
+        Some(IdlSeed::Const(IdlSeedConst {
+            value: serde_json::Value::String(value_string),
+            ty: IdlType::String,
+        }))
+    }
+
+    fn is_instruction(&self, seed_path: &SeedPath) -> bool {
+        self.ix_args.contains_key(&seed_path.name())
+    }
+
+    fn is_const(&self, seed_path: &SeedPath) -> bool {
+        self.const_names.contains(&seed_path.name())
+    }
+
+    fn is_account(&self, seed_path: &SeedPath) -> bool {
+        self.account_field_names.contains(&seed_path.name())
+    }
+
+    fn is_str_literal(&self, seed_path: &SeedPath) -> bool {
+        seed_path.components().is_empty() && seed_path.name().contains('"')
     }
 }
 
@@ -244,6 +270,7 @@ fn parse_seed_path(seed: &Expr) -> Option<SeedPath> {
     // Convert the seed into the raw string representation.
     let seed_str = parser::tts_to_string(&seed);
 
+    // Break up the seed into each sub field component.
     let mut components: Vec<&str> = seed_str.split(" . ").collect();
     if components.len() <= 1 {
         println!("WARNING: seeds are in an unexpected format: {:?}", seed);
@@ -267,57 +294,6 @@ fn parse_seed_path(seed: &Expr) -> Option<SeedPath> {
     }
 
     Some(SeedPath(name, path))
-}
-
-fn parse_seed_account_ty(
-    _ctx: &CrateContext,
-    accounts: &AccountsStruct,
-    var_name: String,
-) -> Option<String> {
-    // Get the anchor account field from the derive accounts struct.
-    let account_field = accounts
-        .fields
-        .iter()
-        .find(|field| *field.ident() == var_name)
-        .unwrap();
-
-    // Get the struct name from the account field.
-    account_field.ty_name()
-}
-
-fn parse_seed_account_field_ty(
-    ctx: &CrateContext,
-    accounts: &AccountsStruct,
-    var_name: String,
-    mut path: &[String],
-) -> Option<IdlType> {
-    match path.len() {
-        0 => Some(IdlType::PublicKey),
-        1 => {
-            // Get the anchor account field from the derive accounts struct.
-            let account_field = accounts
-                .fields
-                .iter()
-                .find(|field| *field.ident() == var_name)
-                .unwrap();
-
-            // Get the struct name from the account field.
-            let ty_name = account_field.ty_name()?;
-
-            if ty_name == "TokenAccount" {
-                assert!(path.len() == 1);
-                let token_field = &path[0];
-                if token_field == "mint" {
-                    return Some(IdlType::PublicKey);
-                }
-            }
-            // Get the rust representation of the field's struct.
-            let strct = ctx.structs().find(|s| s.ident == ty_name).unwrap();
-
-            Some(parse_field_path(ctx, strct, &mut path))
-        }
-        _ => panic!("invariant violation"),
-    }
 }
 
 fn parse_field_path(ctx: &CrateContext, strct: &syn::ItemStruct, path: &mut &[String]) -> IdlType {
