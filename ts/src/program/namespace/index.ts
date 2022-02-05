aba33ace40c82496155e173e7bb8980926324e82
Ashwin Ramaswami <aramaswamis@gmail.com>
2022-02-04 22:52:46 -0500

    Update index.ts

--- ts/src/program/namespace/index.ts
+++ ts/src/program/namespace/index.ts
@@ -14,14 +14,14 @@ import { AllInstructions } from "./types.js";
 import { MethodsBuilderFactory, MethodsNamespace } from "./methods";
 
 // Re-exports.
-export { StateClient } from "./state.js";
-export { InstructionNamespace, InstructionFn } from "./instruction.js";
-export { TransactionNamespace, TransactionFn } from "./transaction.js";
-export { RpcNamespace, RpcFn } from "./rpc.js";
-export { AccountNamespace, AccountClient, ProgramAccount } from "./account.js";
-export { SimulateNamespace, SimulateFn } from "./simulate.js";
-export { IdlAccounts, IdlTypes } from "./types.js";
-export { MethodsBuilderFactory, MethodsNamespace } from "./methods";
+export type { StateClient } from "./state.js";
+export type { InstructionNamespace, InstructionFn } from "./instruction.js";
+export type { TransactionNamespace, TransactionFn } from "./transaction.js";
+export type { RpcNamespace, RpcFn } from "./rpc.js";
+export type { AccountNamespace, AccountClient, ProgramAccount } from "./account.js";
+export type { SimulateNamespace, SimulateFn } from "./simulate.js";
+export type { IdlAccounts, IdlTypes } from "./types.js";
+export type { MethodsBuilderFactory, MethodsNamespace } from "./methods";
 
 export default class NamespaceFactory {
   /**
