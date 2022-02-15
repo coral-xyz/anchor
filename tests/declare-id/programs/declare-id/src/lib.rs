0772e1c706a09e8380a45fc05d92c67cfdb90c39
Armani Ferrante <armaniferrante@gmail.com>
2022-02-15 12:48:18 -0500

    Update tests/declare-id/programs/declare-id/src/lib.rs

--- tests/declare-id/programs/declare-id/src/lib.rs
+++ tests/declare-id/programs/declare-id/src/lib.rs
@@ -1,6 +1,7 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS"); // intentionally different program id than the one defined in Anchor.toml
+// Intentionally different program id than the one defined in Anchor.toml.
+declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
 
 #[program]
