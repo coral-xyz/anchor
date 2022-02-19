8deaeff1e3339d5074f64d33182f6500c9e6719e
Paul Schaaf <paulsimonschaaf@gmail.com>
2022-02-19 19:35:47 +0100

    fix TODOs

--- lang/attribute/account/src/lib.rs
+++ lang/attribute/account/src/lib.rs
@@ -148,11 +148,11 @@ pub fn account(
                 impl #impl_gen anchor_lang::AccountDeserialize for #account_name #type_gen #where_clause {
                     fn try_deserialize(buf: &mut &[u8]) -> anchor_lang::Result<Self> {
                         if buf.len() < #discriminator.len() {
-                            return Err(anchor_lang::anchor_attribute_error::error!(anchor_lang::error::ErrorCode::AccountDiscriminatorNotFound));
+                            return Err(anchor_lang::anchor_attribute_error::error_without_origin!(anchor_lang::error::ErrorCode::AccountDiscriminatorNotFound));
                         }
                         let given_disc = &buf[..8];
                         if &#discriminator != given_disc {
-                            return Err(anchor_lang::anchor_attribute_error::error!(anchor_lang::error::ErrorCode::AccountDiscriminatorMismatch));
+                            return Err(anchor_lang::anchor_attribute_error::error_without_origin!(anchor_lang::error::ErrorCode::AccountDiscriminatorMismatch));
                         }
                         Self::try_deserialize_unchecked(buf)
                     }
@@ -177,11 +177,11 @@ pub fn account(
                 impl #impl_gen anchor_lang::AccountSerialize for #account_name #type_gen #where_clause {
                     fn try_serialize<W: std::io::Write>(&self, writer: &mut W) -> anchor_lang::Result<()> {
                         if writer.write_all(&#discriminator).is_err() {
-                            return Err(anchor_lang::anchor_attribute_error::error!(anchor_lang::error::ErrorCode::AccountDidNotSerialize));
+                            return Err(anchor_lang::anchor_attribute_error::error_without_origin!(anchor_lang::error::ErrorCode::AccountDidNotSerialize));
                         }
 
                         if AnchorSerialize::serialize(self, writer).is_err() {
-                            return Err(anchor_lang::anchor_attribute_error::error!(anchor_lang::error::ErrorCode::AccountDidNotSerialize));
+                            return Err(anchor_lang::anchor_attribute_error::error_without_origin!(anchor_lang::error::ErrorCode::AccountDidNotSerialize));
                         }
                         Ok(())
                     }
@@ -191,20 +191,19 @@ pub fn account(
                 impl #impl_gen anchor_lang::AccountDeserialize for #account_name #type_gen #where_clause {
                     fn try_deserialize(buf: &mut &[u8]) -> anchor_lang::Result<Self> {
                         if buf.len() < #discriminator.len() {
-                            return Err(anchor_lang::anchor_attribute_error::error!(anchor_lang::error::ErrorCode::AccountDiscriminatorNotFound));
+                            return Err(anchor_lang::anchor_attribute_error::error_without_origin!(anchor_lang::error::ErrorCode::AccountDiscriminatorNotFound));
                         }
                         let given_disc = &buf[..8];
                         if &#discriminator != given_disc {
-                            return Err(anchor_lang::anchor_attribute_error::error!(anchor_lang::error::ErrorCode::AccountDiscriminatorMismatch));
+                            return Err(anchor_lang::anchor_attribute_error::error_without_origin!(anchor_lang::error::ErrorCode::AccountDiscriminatorMismatch));
                         }
                         Self::try_deserialize_unchecked(buf)
                     }
 
                     fn try_deserialize_unchecked(buf: &mut &[u8]) -> anchor_lang::Result<Self> {
                         let mut data: &[u8] = &buf[8..];
-                        // TODO: is this really necessary?
                         AnchorDeserialize::deserialize(&mut data)
-                            .map_err(|_| anchor_lang::anchor_attribute_error::error!(anchor_lang::error::ErrorCode::AccountDidNotDeserialize))
+                            .map_err(|_| anchor_lang::anchor_attribute_error::error_without_origin!(anchor_lang::error::ErrorCode::AccountDidNotDeserialize))
                     }
                 }
 
