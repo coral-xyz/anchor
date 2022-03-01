fb8867f48b4e869d7e9661141efd2841a3959d66
Matthew Callens <callensmatt@gmail.com>
2022-03-01 11:00:32 -0500

    update sysvar tests to use methods builder api

--- tests/sysvars/tests/sysvars.js
+++ tests/sysvars/tests/sysvars.js
@@ -7,31 +7,33 @@ describe("sysvars", () => {
   const program = anchor.workspace.Sysvars;
 
   it("Is initialized!", async () => {
-    const tx = await program.rpc.sysvars({
-      accounts: {
+    const tx = await program.methods
+      .sysvars()
+      .accounts({
         clock: anchor.web3.SYSVAR_CLOCK_PUBKEY,
         rent: anchor.web3.SYSVAR_RENT_PUBKEY,
         stakeHistory: anchor.web3.SYSVAR_STAKE_HISTORY_PUBKEY,
-      },
-    });
+      })
+      .rpc();
     console.log("Your transaction signature", tx);
   });
 
-  it('Fails when the wrote pubkeys are provided', async () => {
+  it("Fails when the wrote pubkeys are provided", async () => {
     try {
-      await program.rpc.sysvars({
-        accounts: {
+      await program.methods
+        .sysvars()
+        .accounts({
           clock: anchor.web3.SYSVAR_CLOCK_PUBKEY,
           rent: anchor.web3.SYSVAR_RENT_PUBKEY,
           stakeHistory: anchor.web3.SYSVAR_REWARDS_PUBKEY,
-        },
-      })
-      assert.ok(false)
+        })
+        .rpc();
+      assert.ok(false);
     } catch (err) {
       const errMsg = "The given public key does not match the required sysvar";
       assert.strictEqual(err.toString(), errMsg);
       assert.strictEqual(err.msg, errMsg);
       assert.strictEqual(err.code, 3015);
     }
-  })
+  });
 });
