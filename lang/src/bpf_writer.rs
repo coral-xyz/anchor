b3df851ff825fd68cc4432550ff3f5767656adab
Matthew Callens <callensmatt@gmail.com>
2022-03-09 10:09:56 -0500

    adjust the pos increment to be the remaining writable buffer length

--- lang/src/bpf_writer.rs
+++ lang/src/bpf_writer.rs
@@ -17,9 +17,15 @@ impl<T> BpfWriter<T> {
 impl Write for BpfWriter<&mut [u8]> {
     fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
         let pos = cmp::min(self.pos, self.inner.len() as u64);
-        program_memory::sol_memcpy(&mut self.inner[(pos as usize)..], buf, buf.len());
-        self.pos += buf.len() as u64;
-        Ok(buf.len())
+        let writable_size = self.inner.len().saturating_sub(pos as usize);
+
+        if writable_size == 0 {
+            return Err(io::Error::from(io::ErrorKind::WriteZero));
+        }
+
+        program_memory::sol_memcpy(&mut self.inner[(pos as usize)..], buf, writable_size);
+        self.pos += writable_size as u64;
+        Ok(writable_size)
     }
 
     fn flush(&mut self) -> io::Result<()> {
