88aafe655d24dc4bec56e397e307a2a8134041c7
Tom Linton <tlinton@fastmail.fm>
2022-03-15 09:04:50 +1300

    Use sol_log_data in tests

--- tests/cpi-returns/programs/caller/src/lib.rs
+++ tests/cpi-returns/programs/caller/src/lib.rs
@@ -9,42 +9,42 @@ declare_id!("HmbTLCmaGvZhKnn1Zfa1JVnp7vkMV4DYVxPLWBVoN65L");
 pub mod caller {
     use super::*;
 
-    pub fn cpi_call_return_u64(ctx: Context<CpiReturnU64>) -> Result<()> {
+    pub fn cpi_call_return_u64(ctx: Context<CpiReturnContext>) -> Result<()> {
         let cpi_program = ctx.accounts.cpi_return_program.to_account_info();
         let cpi_accounts = CpiReturn {
             account: ctx.accounts.cpi_return.to_account_info(),
         };
         let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
         let result = callee::cpi::return_u64(cpi_ctx)?;
-        let mut buffer: Vec<u8> = Vec::new();
-        result.serialize(&mut buffer)?;
-        msg!(&base64::encode(buffer));
+        anchor_lang::solana_program::log::sol_log_data(&[&result.try_to_vec().unwrap()]);
         Ok(())
     }
 
-    pub fn cpi_call_return_struct(ctx: Context<CpiReturnStruct>) -> Result<()> {
+    pub fn cpi_call_return_struct(ctx: Context<CpiReturnContext>) -> Result<()> {
         let cpi_program = ctx.accounts.cpi_return_program.to_account_info();
         let cpi_accounts = CpiReturn {
             account: ctx.accounts.cpi_return.to_account_info(),
         };
         let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
         let result = callee::cpi::return_struct(cpi_ctx)?;
-        let mut buffer: Vec<u8> = Vec::new();
-        result.serialize(&mut buffer)?;
-        msg!(&base64::encode(buffer));
+        anchor_lang::solana_program::log::sol_log_data(&[&result.try_to_vec().unwrap()]);
+        Ok(())
+    }
+
+    pub fn cpi_call_return_vec(ctx: Context<CpiReturnContext>) -> Result<()> {
+        let cpi_program = ctx.accounts.cpi_return_program.to_account_info();
+        let cpi_accounts = CpiReturn {
+            account: ctx.accounts.cpi_return.to_account_info(),
+        };
+        let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
+        let result = callee::cpi::return_vec(cpi_ctx)?;
+        anchor_lang::solana_program::log::sol_log_data(&[&result.try_to_vec().unwrap()]);
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct CpiReturnU64<'info> {
-    #[account(mut)]
-    pub cpi_return: Account<'info, CpiReturnAccount>,
-    pub cpi_return_program: Program<'info, Callee>,
-}
-
-#[derive(Accounts)]
-pub struct CpiReturnStruct<'info> {
+pub struct CpiReturnContext<'info> {
     #[account(mut)]
     pub cpi_return: Account<'info, CpiReturnAccount>,
     pub cpi_return_program: Program<'info, Callee>,
