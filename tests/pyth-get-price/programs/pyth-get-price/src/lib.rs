aa9ab558eb2773d546e7415fc8a7930d13bb8898
utx0 <utx0@mettadata.com>
2022-03-17 18:09:07 +1100

    basic pyth working in anchor example.

--- tests/pyth-get-price/programs/pyth-get-price/src/lib.rs
+++ tests/pyth-get-price/programs/pyth-get-price/src/lib.rs
@@ -1,20 +1,62 @@
 use anchor_lang::prelude::*;
 
-declare_id!("EVFzuVuE2DGP7VVAFafVq4knkQ4dqF3mmCtdMcpNLoSA");
+declare_id!("D46LzWxNfmtAL6H2Dcn9uZVxjauKGhYVm9kCd3UPje2h");
 
+/// Converted https://github.com/pyth-network/pyth-examples/blob/main/program/src/lib.rs to anchor
 #[program]
 pub mod pyth_get_price {
-    use pyth_client::{load_price, Price};
     use super::*;
 
     pub fn get_price(ctx: Context<PythGetPrice>) -> Result<()> {
+        // setup accounts
+        let pyth_product_info = &ctx.accounts.pyth_product;
+        let pyth_product_data = &pyth_product_info.try_borrow_data()?;
+        let product_account = *pyth_client::load_product(pyth_product_data).unwrap();
+
+        // security checks
+        if product_account.magic != pyth_client::MAGIC {
+            msg!("Pyth product account provided is not a valid Pyth account");
+            return Err(ProgramError::InvalidArgument.into());
+        }
+
+        if product_account.atype != pyth_client::AccountType::Product as u32 {
+            msg!("Pyth product account provided is not a valid Pyth product account");
+            return Err(ProgramError::InvalidArgument.into());
+        }
+
+        if product_account.ver != pyth_client::VERSION_2 {
+            msg!("Pyth product account provided has a different version than the Pyth client");
+            return Err(ProgramError::InvalidArgument.into());
+        }
+
+        if !product_account.px_acc.is_valid() {
+            msg!("Pyth product price account is invalid");
+            return Err(ProgramError::InvalidArgument.into());
+        }
+
+        let pyth_price_pubkey = Pubkey::new(&product_account.px_acc.val);
+        if &pyth_price_pubkey != &ctx.accounts.pyth_price.key() {
+            msg!("Pyth product price account does not match the Pyth price provided");
+            return Err(ProgramError::InvalidArgument.into());
+        }
+
         let pyth_price_info = &ctx.accounts.pyth_price;
         let pyth_price_data = &pyth_price_info.try_borrow_data()?;
-        let price_account: Price = *load_price(pyth_price_data).unwrap();
+        let price_account = *pyth_client::load_price(pyth_price_data).unwrap();
 
-        msg!("price: {}", price_account.agg.price);
-        msg!("price: {:?}", price_account.get_current_price().unwrap());
-        msg!("status: {:?}", price_account.get_current_price_status());
+
+        msg!("  price_account .. {:?}", pyth_price_info.key);
+        msg!("    price_type ... {:?}", price_account.ptype);
+        msg!("    price ........ {}", price_account.agg.price);
+
+        // TODO: These are all thowing onchain errors:
+        // Program failed to complete: Access violation in stack frame 5 at address 0x200005fd0 of size 8 by instruction #3018
+        // msg!("    exponent ..... {}", price_account.expo);
+        // msg!("    status ....... {:?}", price_account.agg.status);
+        // msg!("    corp_act ..... {:?}",price_account.agg.corp_act);
+        // msg!("    conf ......... {}", price_account.agg.conf);
+        // msg!("    valid_slot ... {}", price_account.valid_slot);
+        // msg!("    publish_slot . {}", price_account.agg.pub_slot);
 
         Ok(())
     }
--- tests/pyth-get-price/tests/pyth-get-price.ts
+++ tests/pyth-get-price/tests/pyth-get-price.ts
@@ -1,12 +1,16 @@
 import * as anchor from "@project-serum/anchor";
 import {Program} from "@project-serum/anchor";
 import {PythGetPrice} from "../target/types/pyth_get_price";
+import {assert} from "chai";
 
 // Devnet accounts https://pyth.network/markets/?cluster=devnet#Crypto.SOL/USD
 const SolProductAccount = new anchor.web3.PublicKey("3Mnn2fX6rQyUsyELYms1sBJyChWofzSNRoqYzvgMVz5E");
-const SolPriceAccount = new anchor.web3.PublicKey("J83w4HKfqxwcq3BEMMkPFSppX3gqekLyLJBexebFVkix")
+const SolPriceAccount = new anchor.web3.PublicKey("J83w4HKfqxwcq3BEMMkPFSppX3gqekLyLJBexebFVkix");
 
+const BtcProductAccount = new anchor.web3.PublicKey("3m1y5h2uv7EQL3KaJZehvAJa4yDNvgc5yAdL9KPMKwvk");
+const BtcPriceAccount = new anchor.web3.PublicKey("HovQMDrbAgAYPCmHVSrezcSmkMtXSSUsLDFANExrZh2J");
 
+// TODO: workout how to run pyth tests on localnet
 
 describe("pyth-get-price", () => {
     // Configure the client to use the local cluster.
@@ -14,20 +18,38 @@ describe("pyth-get-price", () => {
 
     const program = anchor.workspace.PythGetPrice as Program<PythGetPrice>;
 
-    it("Is initialized!", async () => {
-        // Add your test here.
-        const tx = await program.rpc.initialize({});
-        console.log("Your transaction signature", tx);
-    });
-
-
-    it('should get the SOL/USD price from pyth', async () => {
+    it('should print the SOL/USD price from pyth', async () => {
         const tx = await program.rpc.getPrice({
             accounts: {
                 pythProduct: SolProductAccount,
                 pythPrice: SolPriceAccount,
             }
         });
-        console.log("Your transaction signature",tx)
+        console.log("Your transaction signature", tx)
+    });
+
+    it('should price the BTC/USD price from pyth', async () => {
+        const tx = await program.rpc.getPrice({
+            accounts: {
+                pythProduct: BtcProductAccount,
+                pythPrice: BtcPriceAccount,
+            }
+        });
+        console.log("Your transaction signature", tx)
+    });
+
+    it('should not work for incorrect accounts', async () => {
+        try {
+            await program.rpc.getPrice({
+                accounts: {
+                    pythProduct: BtcProductAccount,
+                    pythPrice: SolPriceAccount,
+                }
+            });
+            assert.ok(false, "No error was thrown")
+        } catch (err) {
+            console.log(err.toString())
+            assert.equal(err.toString(),"Error: failed to send transaction: Transaction simulation failed: Error processing Instruction 0: invalid program argument")
+        }
     });
 });
