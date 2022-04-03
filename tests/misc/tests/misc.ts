adb90c33d77cf584dbdd630ae753d2b845c20ebe
Paul <paulsimonschaaf@gmail.com>
2022-04-02 22:27:39 -0400

    lang: Make Anchor use fallback function instead of panicking if ix data.len() is < `8` (#1721)

--- tests/misc/tests/misc.ts
+++ tests/misc/tests/misc.ts
@@ -160,7 +160,7 @@ describe("misc", () => {
       "Program data: jvbowsvlmkcJAAAA",
       "Program data: zxM5neEnS1kBAgMEBQYHCAkK",
       "Program data: g06Ei2GL1gIBAgMEBQYHCAkKCw==",
-      "Program 3TEqcc8xhrhdspwbvoamUJe2borm4Nr72JxL66k6rgrh consumed 5395 of 1400000 compute units",
+      "Program 3TEqcc8xhrhdspwbvoamUJe2borm4Nr72JxL66k6rgrh consumed 5418 of 1400000 compute units",
       "Program 3TEqcc8xhrhdspwbvoamUJe2borm4Nr72JxL66k6rgrh success",
     ];
 
