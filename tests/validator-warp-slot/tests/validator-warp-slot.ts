003fda6fe0e4330c78e2cfd692aa5af3db810b10
skrrb <skrrb@pm.me>
2022-04-04 17:07:10 +0200

    test

---
+++ tests/validator-warp-slot/tests/validator-warp-slot.ts
@@ -0,0 +1,30 @@
+import * as anchor from "@project-serum/anchor";
+import { Program } from "@project-serum/anchor";
+import { ValidatorWarpSlot } from "../target/types/validator_warp_slot";
+
+describe("validator-warp-slot", () => {
+  // Configure the client to use the local cluster.
+  anchor.setProvider(anchor.Provider.env());
+
+  const program = anchor.workspace
+    .ValidatorWarpSlot as Program<ValidatorWarpSlot>;
+
+  it("Is initialized!", async () => {
+    const mainnetConnection = new anchor.web3.Connection(
+      "https://api.mainnet-beta.solana.com"
+    );
+
+    const [clockInfo] = await anchor.utils.rpc.getMultipleAccounts(
+      mainnetConnection,
+      [anchor.web3.SYSVAR_CLOCK_PUBKEY]
+    );
+
+    if (!clockInfo) {
+      throw new Error("Unable to fetch clock account from mainnet");
+    }
+
+    const mainnetSlot = new anchor.BN(clockInfo.account.data.slice(0, 8), "le");
+    const tx = await program.methods.initialize(mainnetSlot).rpc();
+    console.log("Your transaction signature", tx);
+  });
+});
