e015f28ab6dafe687b7c1ac81ef20e100153aac1
Paul Schaaf <paulsimonschaaf@gmail.com>
2022-04-18 15:38:20 -0400

    format comment

--- lang/syn/src/parser/accounts/mod.rs
+++ lang/syn/src/parser/accounts/mod.rs
@@ -121,10 +121,9 @@ fn constraints_cross_checks(fields: &[AccountField]) -> ParseResult<()> {
                 }
             }
             match kind {
-                // this doesn't catch cases like
-                // account.key() or account.key but
-                // my guess is that doesn't happen often and we
-                // can revisit this if I'm wrong
+                // This doesn't catch cases like account.key() or account.key.
+                // My guess is that doesn't happen often and we can revisit
+                // this if I'm wrong.
                 InitKind::Token { mint, .. } | InitKind::AssociatedToken { mint, .. } => {
                     if !fields.iter().any(|f| {
                         f.ident()
