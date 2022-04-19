cf424a134b9411ce6410474e1be096bb589eaca7
Paul Schaaf <paulsimonschaaf@gmail.com>
2022-04-19 15:08:45 -0400

    ts: add metadata field

--- ts/src/idl.ts
+++ ts/src/idl.ts
@@ -14,6 +14,7 @@ export type RawIdl = {
   events?: RawIdlEvent[];
   errors?: IdlErrorCode[];
   constants?: RawIdlConstant[];
+  metadata?: IdlMetadata;
 };
 
 export type Idl = {
@@ -570,6 +571,7 @@ export function camelCaseIdl<IDL extends Idl = Idl>(rawIdl: RawIdl): IDL {
     errors: rawIdl.errors,
     constants: camelCaseConstants(rawIdl.constants),
     camelized: true,
+    metadata: rawIdl.metadata,
   } as IDL;
 }
 
