c6db56090fb838fc188155ffa54728ada9f5bee9
Tom Linton <tlinton@fastmail.fm>
2022-04-21 00:14:21 +1200

    Use --bpf-program instead of deploy

--- tests/anchor-cli-idl/test.sh
+++ tests/anchor-cli-idl/test.sh
@@ -4,15 +4,20 @@
 mkdir -p target/deploy
 cp keypairs/idl_commands_one-keypair.json target/deploy
 
+echo "Building programs"
+
+anchor build
+
 echo "Starting local validator for test"
 
-solana-test-validator --reset -q --mint tgyXxAhCkpgtKCEi4W6xWJSzqwVGs3uk2RodbZP2J49 &
+solana-test-validator --reset \
+  -q \
+  --mint tgyXxAhCkpgtKCEi4W6xWJSzqwVGs3uk2RodbZP2J49 \
+  --bpf-program 2uA3amp95zsEHUpo8qnLMhcFAUsiKVEcKHXS1JetFjU5 target/deploy/idl_commands_one.so \
+  --bpf-program DE4UbHnAcT6Kfh1fVTPRPwpiA3vipmQ4xR3gcLwX3wwS target/deploy/idl_commands_one.so \
+  &
 
 sleep 10
-
-echo "Building and deploying programs"
-
-anchor build && anchor deploy
 
 echo "Running tests"
 
