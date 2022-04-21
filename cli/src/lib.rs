fa3e7c69dd683f88c1c2b24d7d8f9fd8cc3c605e
Blas Rodriguez Irizar <rodrigblas@gmail.com>
2022-04-21 22:06:50 +0200

    cli: add short alias for build and test commands
    Following cargo, one can say:
    cargo build ... OR cargo b ...
    cargo test ... OR cargo t ...
    
    The idea is to be able to mimic this behavior with the anchor
    cli

--- cli/src/lib.rs
+++ cli/src/lib.rs
@@ -71,6 +71,7 @@ pub enum Command {
         no_git: bool,
     },
     /// Builds the workspace.
+    #[clap(name = "build", alias = "b")]
     Build {
         /// Output directory for the IDL.
         #[clap(short, long)]
@@ -154,6 +155,7 @@ pub enum Command {
         )]
         cargo_args: Vec<String>,
     },
+    #[clap(name = "test", alias = "t")]
     /// Runs integration tests against a localnetwork.
     Test {
         /// Use this flag if you want to run tests against previously deployed
