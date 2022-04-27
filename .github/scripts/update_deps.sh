b3b08eea548d788043ba055db22e3b0081bbf2c5
nickgarfield <nick@cronos.so>
2022-04-27 14:14:35 -0500

    Use strict semvar versioning

--- .github/scripts/update_deps.sh
+++ .github/scripts/update_deps.sh
@@ -9,20 +9,20 @@ git merge master
 
 # Update Solana dependencies
 tomls=($(find "." -name Cargo.toml))
-sed -i -e "s#\(solana-program = \"\)[^\"]*\(\"\)#\1$solana_version\2#g" "${tomls[@]}"
-sed -i -e "s#\(solana-program-test = \"\)[^\"]*\(\"\)#\1$solana_version\2#g" "${tomls[@]}"
-sed -i -e "s#\(solana-sdk = \"\).*\(\"\)#\1$solana_version\2#g" "${tomls[@]}"
-sed -i -e "s#\(solana-sdk = { version = \"\)[^\"]*\(\"\)#\1$solana_version\2#g" "${tomls[@]}"
-sed -i -e "s#\(solana-client = \"\)[^\"]*\(\"\)#\1$solana_version\2#g" "${tomls[@]}"
-sed -i -e "s#\(solana-client = { version = \"\)[^\"]*\(\"\)#\1$solana_version\2#g" "${tomls[@]}"
-sed -i -e "s#\(solana-clap-utils = \"\)[^\"]*\(\"\)#\1$solana_version\2#g" "${tomls[@]}"
-sed -i -e "s#\(solana-clap-utils = { version = \"\)[^\"]*\(\"\)#\1$solana_version\2#g" "${tomls[@]}"
-sed -i -e "s#\(solana-cli-config = \"\)[^\"]*\(\"\)#\1$solana_version\2#g" "${tomls[@]}"
-sed -i -e "s#\(solana-cli-config = { version = \"\)[^\"]*\(\"\)#\1$solana_version\2#g" "${tomls[@]}"
-sed -i -e "s#\(solana-account-decoder = \"\)[^\"]*\(\"\)#\1$solana_version\2#g" "${tomls[@]}"
-sed -i -e "s#\(solana-account-decoder = { version = \"\)[^\"]*\(\"\)#\1$solana_version\2#g" "${tomls[@]}"
-sed -i -e "s#\(solana-faucet = \"\)[^\"]*\(\"\)#\1$solana_version\2#g" "${tomls[@]}"
-sed -i -e "s#\(solana-faucet = { version = \"\)[^\"]*\(\"\)#\1$solana_version\2#g" "${tomls[@]}"
+sed -i -e "s#\(solana-program = \"\)[^\"]*\(\"\)#\1=$solana_version\2#g" "${tomls[@]}"
+sed -i -e "s#\(solana-program-test = \"\)[^\"]*\(\"\)#\1=$solana_version\2#g" "${tomls[@]}"
+sed -i -e "s#\(solana-sdk = \"\).*\(\"\)#\1=$solana_version\2#g" "${tomls[@]}"
+sed -i -e "s#\(solana-sdk = { version = \"\)[^\"]*\(\"\)#\1=$solana_version\2#g" "${tomls[@]}"
+sed -i -e "s#\(solana-client = \"\)[^\"]*\(\"\)#\1=$solana_version\2#g" "${tomls[@]}"
+sed -i -e "s#\(solana-client = { version = \"\)[^\"]*\(\"\)#\1=$solana_version\2#g" "${tomls[@]}"
+sed -i -e "s#\(solana-clap-utils = \"\)[^\"]*\(\"\)#\1=$solana_version\2#g" "${tomls[@]}"
+sed -i -e "s#\(solana-clap-utils = { version = \"\)[^\"]*\(\"\)#\1=$solana_version\2#g" "${tomls[@]}"
+sed -i -e "s#\(solana-cli-config = \"\)[^\"]*\(\"\)#\1=$solana_version\2#g" "${tomls[@]}"
+sed -i -e "s#\(solana-cli-config = { version = \"\)[^\"]*\(\"\)#\1=$solana_version\2#g" "${tomls[@]}"
+sed -i -e "s#\(solana-account-decoder = \"\)[^\"]*\(\"\)#\1=$solana_version\2#g" "${tomls[@]}"
+sed -i -e "s#\(solana-account-decoder = { version = \"\)[^\"]*\(\"\)#\1=$solana_version\2#g" "${tomls[@]}"
+sed -i -e "s#\(solana-faucet = \"\)[^\"]*\(\"\)#\1=$solana_version\2#g" "${tomls[@]}"
+sed -i -e "s#\(solana-faucet = { version = \"\)[^\"]*\(\"\)#\1=$solana_version\2#g" "${tomls[@]}"
 
 # Commit changes
 git add .
