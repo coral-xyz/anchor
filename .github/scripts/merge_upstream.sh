85225b5671b94fd41482157660536441aac551e5
nickgarfield <nick@cronos.so>
2022-04-27 14:07:30 -0500

    Update github config settings

--- .github/scripts/merge_upstream.sh
+++ .github/scripts/merge_upstream.sh
@@ -1,7 +1,3 @@
-# Git config
-git config --global user.email "elias@cronos.so"
-git config --global user.name "eliascm17"
-
 # Merge upstream into master
 git remote add upstream https://github.com/project-serum/anchor.git
 git pull upstream master --no-ff
