8fd699ef3c524f21e82fed3c7b0dee4ec5e2a176
nickgarfield <nick@cronos.so>
2022-04-27 11:42:58 -0500

    Update merge-upstream script

--- .github/scripts/merge_upstream.sh
+++ .github/scripts/merge_upstream.sh
@@ -3,15 +3,15 @@ update_solana_dependencies() {
   echo $PWD
 }
 
-git config --global user.email "nick@cronos.so"
-git config --global user.name "nickgarfield"
-git remote add upstream https://github.com/project-serum/anchor.git
+git config --global user.email "Elias.cmoreno17@gmail.com"
+git config --global user.name "Eliascm17"
+# git remote add upstream https://github.com/project-serum/anchor.git
 git pull upstream master --no-ff
 git push origin master
 
 //
 
-git checkout -b nick-mybranch
+git checkout -b latest
 git merge master
 
 solana_version=$(curl -sL https://api.github.com/repos/solana-labs/solana/releases | jq -r 'map(select(.name | startswith("Testnet"))) | first | .tag_name')
