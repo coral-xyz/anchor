cc2cfb4acaa397c3fa491965f89b54964cd3e1a0
nickgarfield <nick@cronos.so>
2022-04-27 14:01:25 -0500

    Update github workflow scripts

--- .github/scripts/merge_upstream.sh
+++ .github/scripts/merge_upstream.sh
@@ -1,24 +1,8 @@
+# Git config
+git config --global user.email "elias@cronos.so"
+git config --global user.name "eliascm17"
 
-update_solana_dependencies() {
-  echo $PWD
-}
-
-git config --global user.email "Elias.cmoreno17@gmail.com"
-git config --global user.name "Eliascm17"
-# git remote add upstream https://github.com/project-serum/anchor.git
+# Merge upstream into master
+git remote add upstream https://github.com/project-serum/anchor.git
 git pull upstream master --no-ff
 git push origin master
-
-//
-
-git checkout -b latest
-git merge master
-
-solana_version=$(curl -sL https://api.github.com/repos/solana-labs/solana/releases | jq -r 'map(select(.name | startswith("Testnet"))) | first | .tag_name')
-solana_ver_cut=$(echo $solana_ver | cut -d'v' -f 2)
-
-update_solana_dependencies . $solana_ver_cut
-
-git commit -m 'updating solana deps'
-
-
---
+++ .github/scripts/update_deps.sh
@@ -0,0 +1,30 @@
+# Get versions
+anchor_version=$(cat VERSION)
+solana_version=$(curl -sL https://api.github.com/repos/solana-labs/solana/releases | jq -r 'map(select(.name | startswith("Testnet"))) | first | .tag_name' | cut -d'v' -f 2)
+branch_name=v$anchor_version-solana.$solana_version
+
+# Checkout new branch
+git checkout -b $branch_name
+git merge master
+
+# Update Solana dependencies
+tomls=($(find "." -name Cargo.toml))
+sed -i -e "s#\(solana-program = \"\)[^\"]*\(\"\)#\1$solana_version\2#g" "${tomls[@]}"
+sed -i -e "s#\(solana-program-test = \"\)[^\"]*\(\"\)#\1$solana_version\2#g" "${tomls[@]}"
+sed -i -e "s#\(solana-sdk = \"\).*\(\"\)#\1$solana_version\2#g" "${tomls[@]}"
+sed -i -e "s#\(solana-sdk = { version = \"\)[^\"]*\(\"\)#\1$solana_version\2#g" "${tomls[@]}"
+sed -i -e "s#\(solana-client = \"\)[^\"]*\(\"\)#\1$solana_version\2#g" "${tomls[@]}"
+sed -i -e "s#\(solana-client = { version = \"\)[^\"]*\(\"\)#\1$solana_version\2#g" "${tomls[@]}"
+sed -i -e "s#\(solana-clap-utils = \"\)[^\"]*\(\"\)#\1$solana_version\2#g" "${tomls[@]}"
+sed -i -e "s#\(solana-clap-utils = { version = \"\)[^\"]*\(\"\)#\1$solana_version\2#g" "${tomls[@]}"
+sed -i -e "s#\(solana-cli-config = \"\)[^\"]*\(\"\)#\1$solana_version\2#g" "${tomls[@]}"
+sed -i -e "s#\(solana-cli-config = { version = \"\)[^\"]*\(\"\)#\1$solana_version\2#g" "${tomls[@]}"
+sed -i -e "s#\(solana-account-decoder = \"\)[^\"]*\(\"\)#\1$solana_version\2#g" "${tomls[@]}"
+sed -i -e "s#\(solana-account-decoder = { version = \"\)[^\"]*\(\"\)#\1$solana_version\2#g" "${tomls[@]}"
+sed -i -e "s#\(solana-faucet = \"\)[^\"]*\(\"\)#\1$solana_version\2#g" "${tomls[@]}"
+sed -i -e "s#\(solana-faucet = { version = \"\)[^\"]*\(\"\)#\1$solana_version\2#g" "${tomls[@]}"
+
+# Commit changes
+git add .
+git commit -m "Bump Solana dependencies to v$solana_version" 
+git push -f origin $branch_name
