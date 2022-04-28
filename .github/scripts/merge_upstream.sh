f60b9ec2a997d3ca2ad40beebc783e65a58ff625
nickgarfield <nick@cronos.so>
2022-04-28 10:04:56 -0500

    Push tags when merging upstream

--- .github/scripts/merge_upstream.sh
+++ .github/scripts/merge_upstream.sh
@@ -1,4 +1,4 @@
 # Merge upstream into master
 git remote add upstream https://github.com/project-serum/anchor.git
 git pull upstream master --no-ff --tags
-git push origin master
+git push origin master --tags
