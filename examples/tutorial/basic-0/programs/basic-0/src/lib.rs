eba4ffc3b58a0424041650dd04e377fe9592434e
Alexander Nitiola <cre8tor.alexander@gmail.com>
2022-05-08 20:40:44 +0100

    Tested basic-0 using solana-local-validator and it passed

--- examples/tutorial/basic-0/programs/basic-0/src/lib.rs
+++ examples/tutorial/basic-0/programs/basic-0/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("D6cG3NZZXDZxbVdgVvN8V7oWym47Rja5itQna2mMt3ha");
 
 #[program]
 mod basic_0 {
