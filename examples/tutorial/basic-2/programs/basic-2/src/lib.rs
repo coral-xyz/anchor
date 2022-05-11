82241b71f4f89b0acd460406d91beff863b94299
Alexander Nitiola <cre8tor.alexander@gmail.com>
2022-05-11 15:29:56 +0100

    Refactored basic-2 tests depreciated methods

--- examples/tutorial/basic-2/programs/basic-2/src/lib.rs
+++ examples/tutorial/basic-2/programs/basic-2/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("Dp31weYrdGEuiRnvnx4wt9pPDtiHwDxagKbhUNM73DeP");
 
 #[program]
 mod basic_2 {
