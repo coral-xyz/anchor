e322dce3638d6e4e4d02947ccca472f6afe0d014
Tristyn <tristynstimpson@gmail.com>
2022-05-23 14:12:00 +0300

    Fix not beng able to import Wallet interface

--- ts/src/index.ts
+++ ts/src/index.ts
@@ -1,13 +1,15 @@
-import NodeWallet from "./nodewallet";
+import { Program } from "./program/index.js";
 import { isBrowser } from "./utils/common.js";
 
 export { default as BN } from "bn.js";
 export * as web3 from "@solana/web3.js";
+export { default as NodeWallet } from "./nodewallet";
 export {
   default as Provider,
   getProvider,
   setProvider,
   AnchorProvider,
+  Wallet
 } from "./provider.js";
 export * from "./error.js";
 export { Instruction } from "./coder/borsh/instruction.js";
@@ -17,8 +19,7 @@ export * as utils from "./utils/index.js";
 export * from "./program/index.js";
 export * from "./spl/index.js";
 
-export declare const workspace: any;
-export declare class Wallet extends NodeWallet {}
+export declare const workspace: Record<string, Program>;
 
 if (!isBrowser) {
   exports.workspace = require("./workspace.js").default;
