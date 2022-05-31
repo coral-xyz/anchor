8b955e4ea14c615c9d5b9da6627d98e6db7c8717
Sayantan Karmakar <sayantankarmakar@outlook.com>
2022-05-31 20:42:08 +0530

    added TypesCoder for serum-dex

--- ts/src/coder/serum/index.ts
+++ ts/src/coder/serum/index.ts
@@ -4,17 +4,20 @@ import { SerumAccountsCoder } from "./accounts";
 import { SerumEventCoder } from "./events";
 import { SerumInstructionCoder } from "./instruction";
 import { SerumStateCoder } from "./state";
+import { SerumTypesCoder } from "./types.js";
 
 export class SerumCoder implements Coder {
   readonly instruction: SerumInstructionCoder;
   readonly accounts: SerumAccountsCoder;
   readonly state: SerumStateCoder;
   readonly events: SerumEventCoder;
+  readonly types: SerumTypesCoder;
 
   constructor(idl: Idl) {
     this.instruction = new SerumInstructionCoder(idl);
     this.accounts = new SerumAccountsCoder(idl);
     this.events = new SerumEventCoder(idl);
     this.state = new SerumStateCoder(idl);
+    this.types = new SerumTypesCoder(idl);
   }
 }
