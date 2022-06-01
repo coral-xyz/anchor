a5dbc7b850ab47b68309046036921c50bc678615
Daniel Marin <danielarturomt@gmail.com>
2022-06-01 18:51:44 +0000

    ts: setup associated token program idl (#1939)

---
+++ ts/src/coder/spl-associated-token/accounts.ts
@@ -0,0 +1,42 @@
+import { AccountsCoder } from "../index.js";
+import { Idl, IdlTypeDef } from "../../idl.js";
+import { accountSize } from "../common";
+
+export class SplAssociatedTokenAccountsCoder<A extends string = string>
+  implements AccountsCoder
+{
+  constructor(private idl: Idl) {}
+
+  public async encode<T = any>(accountName: A, account: T): Promise<Buffer> {
+    switch (accountName) {
+      default: {
+        throw new Error(`Invalid account name: ${accountName}`);
+      }
+    }
+  }
+
+  public decode<T = any>(accountName: A, ix: Buffer): T {
+    return this.decodeUnchecked(accountName, ix);
+  }
+
+  public decodeUnchecked<T = any>(accountName: A, ix: Buffer): T {
+    switch (accountName) {
+      default: {
+        throw new Error(`Invalid account name: ${accountName}`);
+      }
+    }
+  }
+
+  // TODO: this won't use the appendData.
+  public memcmp(accountName: A, _appendData?: Buffer): any {
+    switch (accountName) {
+      default: {
+        throw new Error(`Invalid account name: ${accountName}`);
+      }
+    }
+  }
+
+  public size(idlAccount: IdlTypeDef): number {
+    return accountSize(this.idl, idlAccount) ?? 0;
+  }
+}
--- ts/src/spl/index.ts
+++ ts/src/spl/index.ts
@@ -1,4 +1,8 @@
 import { Program, Provider } from "../index.js";
+import {
+  program as associatedTokenProgram,
+  SplAssociatedToken,
+} from "./associated-token.js";
 import { program as tokenProgram, SplToken } from "./token.js";
 
 export { SplToken } from "./token.js";
@@ -6,5 +10,11 @@ export { SplToken } from "./token.js";
 export class Spl {
   public static token(provider?: Provider): Program<SplToken> {
     return tokenProgram(provider);
+  }
+
+  public static associatedToken(
+    provider?: Provider
+  ): Program<SplAssociatedToken> {
+    return associatedTokenProgram(provider);
   }
 }
