0c5d105b0ec4b3d105a6ed4900a4e82e68bf9535
Italo Casas <me@italoacasas.com>
2022-06-24 13:35:42 -0400

    small fix

--- docs/src/components/Logo.jsx
+++ docs/src/components/Logo.jsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 export function Logo() {
   return (
-    <div className="flex items-center gap-2">
+    <div className="hidden min-w-full items-center gap-2 lg:flex">
       <Image src="/logo.png" alt="Logo" width={30} height={30} />
       <span className="text-xl font-semibold">Anchor</span>
     </div>
