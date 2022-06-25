fcc7addc66580fb21bdc9d3da5cd8fcb47833e1a
Matthew Callens <callensmatt@gmail.com>
2022-06-24 22:06:56 -0400

    add theme color for logo text (#2000)

--- docs/src/components/Logo.jsx
+++ docs/src/components/Logo.jsx
@@ -4,7 +4,7 @@ export function Logo() {
   return (
     <div className="hidden min-w-full items-center gap-2 lg:flex">
       <Image src="/logo.png" alt="Logo" width={30} height={30} />
-      <span className="text-xl font-semibold">Anchor</span>
+      <span className="text-xl font-semibold dark:text-white">Anchor</span>
     </div>
   )
 }
