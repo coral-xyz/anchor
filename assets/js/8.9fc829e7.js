6c8db8c5b6dabece0bc98f70985f19f553e57ead
Armani Ferrante <armaniferrante@gmail.com>
2022-07-05 16:51:36 -0400

    Updates

--- assets/js/8.9fc829e7.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{415:function(t,n,e){"use strict";e.r(n);var s=e(29),i=Object(s.a)({},(function(){var t=this.$createElement;return(this._self._c||t)("p",{staticClass:"demo"},[this._v("This is another component")])}),[],!1,null,null,null);n.default=i.exports}}]);
--- assets/js/9.525a5964.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{399:function(t,n,s){"use strict";s.r(n);var e={data:function(){return{msg:"Hello this is <demo-component>"}}},i=s(29),o=Object(i.a)(e,(function(){var t=this.$createElement;return(this._self._c||t)("p",{staticClass:"demo"},[this._v("\n  "+this._s(this.msg)+"\n")])}),[],!1,null,null,null);n.default=o.exports}}]);
