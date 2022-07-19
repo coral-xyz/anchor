d12d2255bb42c6a980c083e8256b62db516ca62f
Pierre <pierre.duval@gadz.org>
2022-07-20 02:08:21 +1000

    fix: return err on test result err (#2068)

--- cli/src/lib.rs
+++ cli/src/lib.rs
@@ -2031,7 +2031,8 @@ fn run_test_suite(
             }
         }
         Err(err) => {
-            println!("Failed to run test: {:#}", err)
+            println!("Failed to run test: {:#}", err);
+            return Err(err);
         }
     }
 
