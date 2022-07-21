coral-xyz/anchor

commit 0011e22b91b1cb3f3dcafab43312b46fcfac4476

add wallet publickey to Provider (#1845)

ts/src/provider.ts
@@ -20,6 +20,7 @@ import {
 
 export default interface Provider {
   readonly connection: Connection;
+  readonly publicKey: PublicKey;
 
   send?(
     tx: Transaction,
@@ -48,6 +49,8 @@ export default interface Provider {
  * by the provider.
  */
 export class AnchorProvider implements Provider {
+  readonly publicKey: PublicKey;
+
   /**
    * @param connection The cluster connection where the program is deployed.
    * @param wallet     The wallet used to pay for and sign all transactions.
@@ -57,7 +60,9 @@ export class AnchorProvider implements Provider {
     readonly connection: Connection,
     readonly wallet: Wallet,
     readonly opts: ConfirmOptions
-  ) {}
+  ) {
+    this.publicKey = wallet.publicKey;
+  }
 
   static defaultOptions(): ConfirmOptions {
     return {

commit ed171b0ac20e1f9ca764b9236d1bdd9d2ad257d0

Fixing breaking change where null or undefined wallet throws an error Change introduced in https://github.com/coral-xyz/anchor/pull/1845 Intention from https://github.com/coral-xyz/anchor/pull/1867 is for optional publicKey

ts/src/provider.ts
@@ -60,7 +60,7 @@ export class AnchorProvider implements Provider {
     readonly wallet: Wallet,
     readonly opts: ConfirmOptions
   ) {
-    this.publicKey = wallet.publicKey;
+    this.publicKey = wallet?.publicKey;
   }
 
   static defaultOptions(): ConfirmOptions {