71d848e8cc53716c047e2401cf7ece7a77d94d12
Sammy <41593264+stegaBOB@users.noreply.github.com>
2022-08-05 00:51:14 -0400

    update tests

--- tests/optional/tests/optional.ts
+++ tests/optional/tests/optional.ts
@@ -1,49 +1,62 @@
 import * as anchor from "@project-serum/anchor";
-import { BN, Program } from "@project-serum/anchor";
+import {Program} from "@project-serum/anchor";
+import {Optional, IDL} from "../target/types/optional";
 import { expect } from 'chai';
-import { Optional } from "../target/types/optional";
+import {SystemProgram} from "@solana/web3.js";
 
 describe("Optional", () => {
-  // configure the client to use the local cluster
-  anchor.setProvider(anchor.AnchorProvider.env());
-  // candy guard for the tests
-  const keypair = anchor.web3.Keypair.generate();
-  // candy guard program
-  const program = anchor.workspace.Optional as Program<Optional>;
-  // payer of the transactions
-  const payer = (program.provider as anchor.AnchorProvider).wallet;
+    // configure the client to use the local cluster
+    anchor.setProvider(anchor.AnchorProvider.env());
 
-  it("initialize", async () => {
-    await program.methods
-        .initialize()
-        .accounts({
-          candyGuard: keypair.publicKey,
-          authority: payer.publicKey,
-          payer: payer.publicKey,
-        })
-        .signers([keypair])
-        .rpc();
-    let data = await program.account.candyGuard.fetch(keypair.publicKey);
+    // let signer = anchor.web3.Keypair.generate();
+    const optional1 = anchor.web3.Keypair.generate();
+    const optional2 = anchor.web3.Keypair.generate();
 
-    expect(candy_guard.features.toNumber()).to.equal(0);
-  });
+    // optional program
+    const program = anchor.workspace.Optional as Program<Optional>;
+    // payer of the transactions
+    const payer = (program.provider as anchor.AnchorProvider).wallet;
 
-  it("update", async () => {
-    let candy_guard = await program.account.candyGuard.fetch(keypair.publicKey);
-    expect(candy_guard.features.toNumber()).to.equal(0);
-    // console.log(program.)
+    it("initialize", async () => {
+        console.log();
+        let txn = await program.methods
+            .initialize(
+                new anchor.BN(10), optional1.publicKey
+            )
+            .accounts({
+                payer: payer.publicKey,
+                // optional1: optional1.publicKey,
+                systemProgram: SystemProgram.programId,
+                optional2: optional2.publicKey,
+            })
+            // .signers([optional1, optional2])
+            .signers([optional2])
+        console.log(JSON.stringify((await txn.transaction()).instructions.map(ix => ix.keys), null, " "));
+        console.log(JSON.stringify(txn._txFn))
+        await txn.rpc();
 
+        let txn2 = await program.methods
+            .initialize(
+                new anchor.BN(10), optional1.publicKey
+            )
+            .accounts({
+                payer: payer.publicKey,
+                optional1: optional1.publicKey,
+                systemProgram: SystemProgram.programId,
+                // optional2: optional2.publicKey,
+            })
+            .signers([optional1])
+        console.log(JSON.stringify((await txn2.transaction()).instructions.map(ix => ix.keys), null, " "));
 
-    await program.methods.update(settings).accounts({
-      // candyGuard: null,
-      authority: null,
-      candyGuard: keypair.publicKey,
-      // authority: payer.publicKey,
-    }).rpc();
+        // .signers([optional1, optional2])
+            // .signers([optional2])
+        // let data1 = await program.account.data1.fetch(optional1.publicKey);
+        // console.log(data1)
+        let data2 = await program.account.data2.fetch(optional2.publicKey);
+        console.log(data2)
+        //
+        // // expect(data1.data.toNumber()).to.equal(10);
+        expect(data2.optional1.toString()).to.equal(optional1.publicKey.toString());
+    });
 
-    candy_guard = await program.account.candyGuard.fetch(keypair.publicKey);
-    // bot_tax (1) + live_date (2) + lamports_charge (8)
-    console.log(candy_guard.features.toNumber());
-    expect(candy_guard.features.toNumber()).to.equal(11);
-  });
 });
