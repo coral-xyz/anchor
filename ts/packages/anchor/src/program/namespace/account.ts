363b90656306e2b3116ed9bb1597177704de891b
Niceural <nicolas.bayle20@imperial.ac.uk>
2022-08-10 22:44:10 +0200

    Don't use wallet object for account.createInstruction function 2112

--- ts/packages/anchor/src/program/namespace/account.ts
+++ ts/packages/anchor/src/program/namespace/account.ts
@@ -346,16 +346,8 @@ export class AccountClient<
   ): Promise<TransactionInstruction> {
     const size = this.size;
 
-    // @ts-expect-error
-    if (this._provider.wallet === undefined) {
-      throw new Error(
-        "This function requires the Provider interface implementor to have a 'wallet' field."
-      );
-    }
-
     return SystemProgram.createAccount({
-      // @ts-expect-error
-      fromPubkey: this._provider.wallet.publicKey,
+      fromPubkey: this._provider.publicKey,
       newAccountPubkey: signer.publicKey,
       space: sizeOverride ?? size,
       lamports:
