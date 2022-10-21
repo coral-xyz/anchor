46f0b67bffe4082561ba17104041865a5e382513
Henry-E <henry.elder@adaptcentre.ie>
2022-10-21 10:32:50 +0100

    Revert "updated, outdated methods of basic-0,1,2,3 (#2144)" (#2235)
    This reverts commit 6a46bedaf1892acfef0e5952ae96134e20e91b35.

--- examples/tutorial/basic-3/tests/basic-3.js
+++ examples/tutorial/basic-3/tests/basic-3.js
@@ -14,20 +14,22 @@ describe("basic-3", () => {
 
     // Initialize a new puppet account.
     const newPuppetAccount = anchor.web3.Keypair.generate();
-    const tx = await puppet.methods.initialize().
-      accounts({
+    const tx = await puppet.rpc.initialize({
+      accounts: {
         puppet: newPuppetAccount.publicKey,
         user: provider.wallet.publicKey,
         systemProgram: SystemProgram.programId,
-      }).
-      signers([newPuppetAccount]).rpc();
+      },
+      signers: [newPuppetAccount],
+    });
 
     // Invoke the puppet master to perform a CPI to the puppet.
-    await puppetMaster.methods.pullStrings(new anchor.BN(111)).
-      accounts({
+    await puppetMaster.rpc.pullStrings(new anchor.BN(111), {
+      accounts: {
         puppet: newPuppetAccount.publicKey,
         puppetProgram: puppet.programId,
-      }).rpc();
+      },
+    });
 
     // Check the state updated.
     puppetAccount = await puppet.account.data.fetch(newPuppetAccount.publicKey);
