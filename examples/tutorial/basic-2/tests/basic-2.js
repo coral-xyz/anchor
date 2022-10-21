e0d6d8951eea1638089f65f1277abfe4f34fe307
Henry-E <henry.elder@adaptcentre.ie>
2022-10-21 10:32:50 +0100

    Revert "updated, outdated methods of basic-0,1,2,3 (#2144)" (#2235)
    This reverts commit 6a46bedaf1892acfef0e5952ae96134e20e91b35.

--- examples/tutorial/basic-2/tests/basic-2.js
+++ examples/tutorial/basic-2/tests/basic-2.js
@@ -15,12 +15,14 @@ describe("basic-2", () => {
   const program = anchor.workspace.Basic2;
 
   it("Creates a counter", async () => {
-    await program.methods.create(provider.wallet.publicKey).
-      accounts({
+    await program.rpc.create(provider.wallet.publicKey, {
+      accounts: {
         counter: counter.publicKey,
         user: provider.wallet.publicKey,
         systemProgram: SystemProgram.programId,
-      }).signers([counter]).rpc();
+      },
+      signers: [counter],
+    });
 
     let counterAccount = await program.account.counter.fetch(counter.publicKey);
 
@@ -29,11 +31,12 @@ describe("basic-2", () => {
   });
 
   it("Updates a counter", async () => {
-     await program.methods.increment().
-      accounts({
+    await program.rpc.increment({
+      accounts: {
         counter: counter.publicKey,
         authority: provider.wallet.publicKey,
-      }).rpc();
+      },
+    });
 
     const counterAccount = await program.account.counter.fetch(
       counter.publicKey
