ae6bd562cb02032d492cf5057a625aee689320c5
Lucas Steuernagel <lucas.tnagel@gmail.com>
2022-11-14 16:11:00 -0300

    Add support for 256-bit integers in the IDL
    Signed-off-by: Lucas Steuernagel <lucas.tnagel@gmail.com>

--- ts/packages/anchor/tests/coder-types.spec.ts
+++ ts/packages/anchor/tests/coder-types.spec.ts
@@ -1,5 +1,6 @@
 import * as assert from "assert";
 import { BorshCoder } from "../src";
+import BN from "bn.js";
 
 describe("coder.types", () => {
   test("Can encode and decode user-defined types", () => {
@@ -41,5 +42,46 @@ describe("coder.types", () => {
     const encoded = coder.types.encode("MintInfo", mintInfo);
 
     assert.deepEqual(coder.types.decode("MintInfo", encoded), mintInfo);
+  });
+
+  test('Can encode and decode 256-bit integers', () => {
+    const idl = {
+      version: "0.0.0",
+      name: "basic_0",
+      instructions: [
+        {
+          name: "initialize",
+          accounts: [],
+          args: [],
+        },
+      ],
+      types: [
+        {
+          name: "IntegerTest",
+          type: {
+            kind: "struct" as const,
+            fields: [
+              {
+                name: "unsigned",
+                type: "u256" as const,
+              },
+              {
+                name: "signed",
+                type: "i256" as const,
+              },
+            ],
+          },
+        },
+      ],
+    };
+
+    const testing = {
+      unsigned: new BN(2588012355),
+      signed: new BN(-93842345),
+    };
+
+    const coder = new BorshCoder(idl);
+    const encoded = coder.types.encode("IntegerTest", testing);
+    assert.strictEqual(coder.types.decode("IntegerTest", encoded).toString(), testing.toString());
   });
 });
