2f9bcbf1b580cba5e56f5ff8b590a91e99177592
Jon Cinque <joncinque@pm.me>
2023-02-04 00:32:15 +0100

    test: Support token or token-2022 in proxy

--- tests/spl/token-proxy/programs/token-proxy/src/lib.rs
+++ tests/spl/token-proxy/programs/token-proxy/src/lib.rs
@@ -1,7 +1,9 @@
 //! This example demonstrates the use of the `anchor_spl::token` CPI client.
 
 use anchor_lang::prelude::*;
-use anchor_spl::token::{self, Burn, MintTo, SetAuthority, Transfer};
+use anchor_spl::token_interface::{
+    self, Burn, Mint, MintTo, SetAuthority, TokenAccount, TokenInterface, Transfer,
+};
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
@@ -10,15 +12,16 @@ mod token_proxy {
     use super::*;
 
     pub fn proxy_transfer(ctx: Context<ProxyTransfer>, amount: u64) -> Result<()> {
-        token::transfer(ctx.accounts.into(), amount)
+        #[allow(deprecated)]
+        token_interface::transfer(ctx.accounts.into(), amount)
     }
 
     pub fn proxy_mint_to(ctx: Context<ProxyMintTo>, amount: u64) -> Result<()> {
-        token::mint_to(ctx.accounts.into(), amount)
+        token_interface::mint_to(ctx.accounts.into(), amount)
     }
 
     pub fn proxy_burn(ctx: Context<ProxyBurn>, amount: u64) -> Result<()> {
-        token::burn(ctx.accounts.into(), amount)
+        token_interface::burn(ctx.accounts.into(), amount)
     }
 
     pub fn proxy_set_authority(
@@ -26,7 +29,7 @@ mod token_proxy {
         authority_type: AuthorityType,
         new_authority: Option<Pubkey>,
     ) -> Result<()> {
-        token::set_authority(ctx.accounts.into(), authority_type.into(), new_authority)
+        token_interface::set_authority(ctx.accounts.into(), authority_type.into(), new_authority)
     }
 }
 
@@ -45,43 +48,48 @@ pub enum AuthorityType {
 #[derive(Accounts)]
 pub struct ProxyTransfer<'info> {
     #[account(signer)]
+    /// CHECK:
     pub authority: AccountInfo<'info>,
     #[account(mut)]
-    pub from: AccountInfo<'info>,
+    pub from: InterfaceAccount<'info, TokenAccount>,
     #[account(mut)]
-    pub to: AccountInfo<'info>,
-    pub token_program: AccountInfo<'info>,
+    pub to: InterfaceAccount<'info, TokenAccount>,
+    pub token_program: Interface<'info, TokenInterface>,
 }
 
 #[derive(Accounts)]
 pub struct ProxyMintTo<'info> {
     #[account(signer)]
+    /// CHECK:
     pub authority: AccountInfo<'info>,
     #[account(mut)]
-    pub mint: AccountInfo<'info>,
+    pub mint: InterfaceAccount<'info, Mint>,
     #[account(mut)]
-    pub to: AccountInfo<'info>,
-    pub token_program: AccountInfo<'info>,
+    pub to: InterfaceAccount<'info, TokenAccount>,
+    pub token_program: Interface<'info, TokenInterface>,
 }
 
 #[derive(Accounts)]
 pub struct ProxyBurn<'info> {
     #[account(signer)]
+    /// CHECK:
     pub authority: AccountInfo<'info>,
     #[account(mut)]
-    pub mint: AccountInfo<'info>,
+    pub mint: InterfaceAccount<'info, Mint>,
     #[account(mut)]
-    pub from: AccountInfo<'info>,
-    pub token_program: AccountInfo<'info>,
+    pub from: InterfaceAccount<'info, TokenAccount>,
+    pub token_program: Interface<'info, TokenInterface>,
 }
 
 #[derive(Accounts)]
 pub struct ProxySetAuthority<'info> {
     #[account(signer)]
+    /// CHECK:
     pub current_authority: AccountInfo<'info>,
     #[account(mut)]
+    /// CHECK:
     pub account_or_mint: AccountInfo<'info>,
-    pub token_program: AccountInfo<'info>,
+    pub token_program: Interface<'info, TokenInterface>,
 }
 
 impl<'a, 'b, 'c, 'info> From<&mut ProxyTransfer<'info>>
@@ -89,11 +97,11 @@ impl<'a, 'b, 'c, 'info> From<&mut ProxyTransfer<'info>>
 {
     fn from(accounts: &mut ProxyTransfer<'info>) -> CpiContext<'a, 'b, 'c, 'info, Transfer<'info>> {
         let cpi_accounts = Transfer {
-            from: accounts.from.clone(),
-            to: accounts.to.clone(),
+            from: accounts.from.to_account_info().clone(),
+            to: accounts.to.to_account_info().clone(),
             authority: accounts.authority.clone(),
         };
-        let cpi_program = accounts.token_program.clone();
+        let cpi_program = accounts.token_program.to_account_info();
         CpiContext::new(cpi_program, cpi_accounts)
     }
 }
@@ -103,11 +111,11 @@ impl<'a, 'b, 'c, 'info> From<&mut ProxyMintTo<'info>>
 {
     fn from(accounts: &mut ProxyMintTo<'info>) -> CpiContext<'a, 'b, 'c, 'info, MintTo<'info>> {
         let cpi_accounts = MintTo {
-            mint: accounts.mint.clone(),
-            to: accounts.to.clone(),
+            mint: accounts.mint.to_account_info().clone(),
+            to: accounts.to.to_account_info().clone(),
             authority: accounts.authority.clone(),
         };
-        let cpi_program = accounts.token_program.clone();
+        let cpi_program = accounts.token_program.to_account_info();
         CpiContext::new(cpi_program, cpi_accounts)
     }
 }
@@ -115,11 +123,11 @@ impl<'a, 'b, 'c, 'info> From<&mut ProxyMintTo<'info>>
 impl<'a, 'b, 'c, 'info> From<&mut ProxyBurn<'info>> for CpiContext<'a, 'b, 'c, 'info, Burn<'info>> {
     fn from(accounts: &mut ProxyBurn<'info>) -> CpiContext<'a, 'b, 'c, 'info, Burn<'info>> {
         let cpi_accounts = Burn {
-            mint: accounts.mint.clone(),
-            from: accounts.from.clone(),
+            mint: accounts.mint.to_account_info().clone(),
+            from: accounts.from.to_account_info().clone(),
             authority: accounts.authority.clone(),
         };
-        let cpi_program = accounts.token_program.clone();
+        let cpi_program = accounts.token_program.to_account_info();
         CpiContext::new(cpi_program, cpi_accounts)
     }
 }
@@ -134,18 +142,20 @@ impl<'a, 'b, 'c, 'info> From<&mut ProxySetAuthority<'info>>
             account_or_mint: accounts.account_or_mint.clone(),
             current_authority: accounts.current_authority.clone(),
         }; // TODO: Support multisig signers
-        let cpi_program = accounts.token_program.clone();
+        let cpi_program = accounts.token_program.to_account_info();
         CpiContext::new(cpi_program, cpi_accounts)
     }
 }
 
-impl From<AuthorityType> for spl_token::instruction::AuthorityType {
-    fn from(authority_ty: AuthorityType) -> spl_token::instruction::AuthorityType {
+impl From<AuthorityType> for spl_token_2022::instruction::AuthorityType {
+    fn from(authority_ty: AuthorityType) -> spl_token_2022::instruction::AuthorityType {
         match authority_ty {
-            AuthorityType::MintTokens => spl_token::instruction::AuthorityType::MintTokens,
-            AuthorityType::FreezeAccount => spl_token::instruction::AuthorityType::FreezeAccount,
-            AuthorityType::AccountOwner => spl_token::instruction::AuthorityType::AccountOwner,
-            AuthorityType::CloseAccount => spl_token::instruction::AuthorityType::CloseAccount,
+            AuthorityType::MintTokens => spl_token_2022::instruction::AuthorityType::MintTokens,
+            AuthorityType::FreezeAccount => {
+                spl_token_2022::instruction::AuthorityType::FreezeAccount
+            }
+            AuthorityType::AccountOwner => spl_token_2022::instruction::AuthorityType::AccountOwner,
+            AuthorityType::CloseAccount => spl_token_2022::instruction::AuthorityType::CloseAccount,
         }
     }
 }
--- tests/spl/token-proxy/tests/token-proxy.js
+++ tests/spl/token-proxy/tests/token-proxy.js
@@ -1,86 +1,117 @@
 const anchor = require("@coral-xyz/anchor");
 const { assert } = require("chai");
+const {
+  splTokenProgram,
+  SPL_TOKEN_PROGRAM_ID,
+} = require("@coral-xyz/spl-token");
 
-describe("token", () => {
+describe("program", () => {
   const provider = anchor.AnchorProvider.local();
+
+  const TEST_PROGRAM_IDS = [
+    SPL_TOKEN_PROGRAM_ID,
+    new anchor.web3.PublicKey("TokenzQdBNbLqP5VEhdkAS6EPFLC1PHnBqCXEpPxuEb"),
+  ];
+  const TOKEN_PROGRAMS = TEST_PROGRAM_IDS.map((programId) =>
+    splTokenProgram({
+      provider,
+      programId,
+    })
+  );
 
   // Configure the client to use the local cluster.
   anchor.setProvider(provider);
 
   const program = anchor.workspace.TokenProxy;
 
-  let mint = null;
-  let from = null;
-  let to = null;
+  TOKEN_PROGRAMS.forEach((tokenProgram) => {
+    const name =
+      tokenProgram.programId === SPL_TOKEN_PROGRAM_ID ? "token" : "token-2022";
+    describe(name, () => {
+      let mint = null;
+      let from = null;
+      let to = null;
 
-  it("Initializes test state", async () => {
-    mint = await createMint(provider);
-    from = await createTokenAccount(provider, mint, provider.wallet.publicKey);
-    to = await createTokenAccount(provider, mint, provider.wallet.publicKey);
-  });
+      it("Initializes test state", async () => {
+        mint = await createMint(tokenProgram);
+        from = await createTokenAccount(
+          tokenProgram,
+          mint,
+          provider.wallet.publicKey
+        );
+        to = await createTokenAccount(
+          tokenProgram,
+          mint,
+          provider.wallet.publicKey
+        );
+      });
 
-  it("Mints a token", async () => {
-    await program.rpc.proxyMintTo(new anchor.BN(1000), {
-      accounts: {
-        authority: provider.wallet.publicKey,
-        mint,
-        to: from,
-        tokenProgram: TokenInstructions.TOKEN_PROGRAM_ID,
-      },
+      it("Mints a token", async () => {
+        await program.rpc.proxyMintTo(new anchor.BN(1000), {
+          accounts: {
+            authority: provider.wallet.publicKey,
+            mint,
+            to: from,
+            tokenProgram: tokenProgram.programId,
+          },
+        });
+
+        const fromAccount = await getTokenAccount(provider, from);
+
+        assert.isTrue(fromAccount.amount.eq(new anchor.BN(1000)));
+      });
+
+      it("Transfers a token", async () => {
+        await program.rpc.proxyTransfer(new anchor.BN(400), {
+          accounts: {
+            authority: provider.wallet.publicKey,
+            to,
+            from,
+            tokenProgram: tokenProgram.programId,
+          },
+        });
+
+        const fromAccount = await getTokenAccount(provider, from);
+        const toAccount = await getTokenAccount(provider, to);
+
+        assert.isTrue(fromAccount.amount.eq(new anchor.BN(600)));
+        assert.isTrue(toAccount.amount.eq(new anchor.BN(400)));
+      });
+
+      it("Burns a token", async () => {
+        await program.rpc.proxyBurn(new anchor.BN(399), {
+          accounts: {
+            authority: provider.wallet.publicKey,
+            mint,
+            from: to,
+            tokenProgram: tokenProgram.programId,
+          },
+        });
+
+        const toAccount = await getTokenAccount(provider, to);
+        assert.isTrue(toAccount.amount.eq(new anchor.BN(1)));
+      });
+
+      it("Set new mint authority", async () => {
+        const newMintAuthority = anchor.web3.Keypair.generate();
+        await program.rpc.proxySetAuthority(
+          { mintTokens: {} },
+          newMintAuthority.publicKey,
+          {
+            accounts: {
+              accountOrMint: mint,
+              currentAuthority: provider.wallet.publicKey,
+              tokenProgram: tokenProgram.programId,
+            },
+          }
+        );
+
+        const mintInfo = await getMintInfo(provider, mint);
+        assert.isTrue(
+          mintInfo.mintAuthority.equals(newMintAuthority.publicKey)
+        );
+      });
     });
-
-    const fromAccount = await getTokenAccount(provider, from);
-
-    assert.isTrue(fromAccount.amount.eq(new anchor.BN(1000)));
-  });
-
-  it("Transfers a token", async () => {
-    await program.rpc.proxyTransfer(new anchor.BN(400), {
-      accounts: {
-        authority: provider.wallet.publicKey,
-        to,
-        from,
-        tokenProgram: TokenInstructions.TOKEN_PROGRAM_ID,
-      },
-    });
-
-    const fromAccount = await getTokenAccount(provider, from);
-    const toAccount = await getTokenAccount(provider, to);
-
-    assert.isTrue(fromAccount.amount.eq(new anchor.BN(600)));
-    assert.isTrue(toAccount.amount.eq(new anchor.BN(400)));
-  });
-
-  it("Burns a token", async () => {
-    await program.rpc.proxyBurn(new anchor.BN(399), {
-      accounts: {
-        authority: provider.wallet.publicKey,
-        mint,
-        from: to,
-        tokenProgram: TokenInstructions.TOKEN_PROGRAM_ID,
-      },
-    });
-
-    const toAccount = await getTokenAccount(provider, to);
-    assert.isTrue(toAccount.amount.eq(new anchor.BN(1)));
-  });
-
-  it("Set new mint authority", async () => {
-    const newMintAuthority = anchor.web3.Keypair.generate();
-    await program.rpc.proxySetAuthority(
-      { mintTokens: {} },
-      newMintAuthority.publicKey,
-      {
-        accounts: {
-          accountOrMint: mint,
-          currentAuthority: provider.wallet.publicKey,
-          tokenProgram: TokenInstructions.TOKEN_PROGRAM_ID,
-        },
-      }
-    );
-
-    const mintInfo = await getMintInfo(provider, mint);
-    assert.isTrue(mintInfo.mintAuthority.equals(newMintAuthority.publicKey));
   });
 });
 
@@ -88,13 +119,6 @@ describe("token", () => {
 // mostly irrelevant to the point of the example.
 
 const serumCmn = require("@project-serum/common");
-const TokenInstructions = require("@project-serum/serum").TokenInstructions;
-
-// TODO: remove this constant once @project-serum/serum uses the same version
-//       of @solana/web3.js as anchor (or switch packages).
-const TOKEN_PROGRAM_ID = new anchor.web3.PublicKey(
-  TokenInstructions.TOKEN_PROGRAM_ID.toString()
-);
 
 async function getTokenAccount(provider, addr) {
   return await serumCmn.getTokenAccount(provider, addr);
@@ -104,75 +128,33 @@ async function getMintInfo(provider, mintAddr) {
   return await serumCmn.getMintInfo(provider, mintAddr);
 }
 
-async function createMint(provider, authority) {
-  if (authority === undefined) {
-    authority = provider.wallet.publicKey;
-  }
+async function createMint(tokenProgram) {
   const mint = anchor.web3.Keypair.generate();
-  const instructions = await createMintInstructions(
-    provider,
-    authority,
-    mint.publicKey
-  );
+  const authority = tokenProgram.provider.wallet.publicKey;
+  const createMintIx = await tokenProgram.account.mint.createInstruction(mint);
+  const initMintIx = await tokenProgram.methods
+    .initializeMint2(0, authority, null)
+    .accounts({ mint: mint.publicKey })
+    .instruction();
 
   const tx = new anchor.web3.Transaction();
-  tx.add(...instructions);
+  tx.add(createMintIx, initMintIx);
 
-  await provider.sendAndConfirm(tx, [mint]);
+  await tokenProgram.provider.sendAndConfirm(tx, [mint]);
 
   return mint.publicKey;
 }
 
-async function createMintInstructions(provider, authority, mint) {
-  let instructions = [
-    anchor.web3.SystemProgram.createAccount({
-      fromPubkey: provider.wallet.publicKey,
-      newAccountPubkey: mint,
-      space: 82,
-      lamports: await provider.connection.getMinimumBalanceForRentExemption(82),
-      programId: TOKEN_PROGRAM_ID,
-    }),
-    TokenInstructions.initializeMint({
-      mint,
-      decimals: 0,
-      mintAuthority: authority,
-    }),
-  ];
-  return instructions;
-}
-
-async function createTokenAccount(provider, mint, owner) {
+async function createTokenAccount(tokenProgram, mint, owner) {
   const vault = anchor.web3.Keypair.generate();
   const tx = new anchor.web3.Transaction();
-  tx.add(
-    ...(await createTokenAccountInstrs(provider, vault.publicKey, mint, owner))
-  );
-  await provider.sendAndConfirm(tx, [vault]);
+  const createTokenAccountIx =
+    await tokenProgram.account.account.createInstruction(vault);
+  const initTokenAccountIx = await tokenProgram.methods
+    .initializeAccount3(owner)
+    .accounts({ account: vault.publicKey, mint })
+    .instruction();
+  tx.add(createTokenAccountIx, initTokenAccountIx);
+  await tokenProgram.provider.sendAndConfirm(tx, [vault]);
   return vault.publicKey;
-}
-
-async function createTokenAccountInstrs(
-  provider,
-  newAccountPubkey,
-  mint,
-  owner,
-  lamports
-) {
-  if (lamports === undefined) {
-    lamports = await provider.connection.getMinimumBalanceForRentExemption(165);
-  }
-  return [
-    anchor.web3.SystemProgram.createAccount({
-      fromPubkey: provider.wallet.publicKey,
-      newAccountPubkey,
-      space: 165,
-      lamports,
-      programId: TOKEN_PROGRAM_ID,
-    }),
-    TokenInstructions.initializeAccount({
-      account: newAccountPubkey,
-      mint,
-      owner,
-    }),
-  ];
 }
