3f4e12df6b377165c9aabc60b62cece2b59c6f43
CanardMandarin <thibault.marboud@gmail.com>
2023-04-19 15:28:18 +0200

    fix: handler name collision

--- lang/syn/src/codegen/program/handlers.rs
+++ lang/syn/src/codegen/program/handlers.rs
@@ -111,8 +111,8 @@ pub fn generate(program: &Program) -> proc_macro2::TokenStream {
             quote! {
                 #[inline(never)]
                 pub fn #ix_method_name(
-                    program_id: &Pubkey,
-                    accounts: &[AccountInfo],
+                    __program_id: &Pubkey,
+                    __accounts: &[AccountInfo],
                     ix_data: &[u8],
                 ) -> anchor_lang::Result<()> {
                     #[cfg(not(feature = "no-log-ix-name"))]
@@ -129,9 +129,9 @@ pub fn generate(program: &Program) -> proc_macro2::TokenStream {
                     let mut __reallocs = std::collections::BTreeSet::new();
 
                     // Deserialize accounts.
-                    let mut remaining_accounts: &[AccountInfo] = accounts;
-                    let mut accounts = #anchor::try_accounts(
-                        program_id,
+                    let mut remaining_accounts: &[AccountInfo] = __accounts;
+                    let mut __accounts = #anchor::try_accounts(
+                        __program_id,
                         &mut remaining_accounts,
                         ix_data,
                         &mut __bumps,
@@ -141,8 +141,8 @@ pub fn generate(program: &Program) -> proc_macro2::TokenStream {
                     // Invoke user defined handler.
                     let result = #program_name::#ix_method_name(
                         anchor_lang::context::Context::new(
-                            program_id,
-                            &mut accounts,
+                            __program_id,
+                            &mut __accounts,
                             remaining_accounts,
                             __bumps,
                         ),
@@ -153,7 +153,7 @@ pub fn generate(program: &Program) -> proc_macro2::TokenStream {
                     #maybe_set_return_data
 
                     // Exit routine.
-                    accounts.exit(program_id)
+                    __accounts.exit(__program_id)
                 }
             }
         })
