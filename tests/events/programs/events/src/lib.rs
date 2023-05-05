e68be791ca9c4a2d0b6abb1cd4b9b4760a45b783
ngundotra <noah@gundotra.org>
2023-05-05 16:57:30 -0400

    fix tests/events

--- tests/events/programs/events/src/lib.rs
+++ tests/events/programs/events/src/lib.rs
@@ -25,15 +25,15 @@ pub mod events {
     }
 
     pub fn test_event_cpi(ctx: Context<TestEventCpi>) -> Result<()> {
-        // emit_cpi!(
-        //     ctx.accounts.program.to_account_info(),
-        //     ctx.accounts.event_authority.to_account_info(),
-        //     *ctx.bumps.get("event_authority").unwrap(),
-        //     MyOtherEvent {
-        //         data: 7,
-        //         label: "cpi".to_string(),
-        //     }
-        // );
+        emit_cpi!(
+            ctx.accounts.program.to_account_info(),
+            ctx.accounts.event_authority.to_account_info(),
+            *ctx.bumps.get("event_authority").unwrap(),
+            MyOtherEvent {
+                data: 7,
+                label: "cpi".to_string(),
+            }
+        );
         Ok(())
     }
 }
