66d72b1631c2b1bf6a2ae19f50c987ef4fb93abb
acheron <acheroncrypto@gmail.com>
2023-05-23 10:30:05 +0200

    Add malicious invocation test

--- tests/events/tests/events.js
+++ tests/events/tests/events.js
@@ -1,85 +1,116 @@
 const anchor = require("@coral-xyz/anchor");
 const { assert } = require("chai");
 
-describe("events", () => {
+describe("Events", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
   const program = anchor.workspace.Events;
 
-  it("Is initialized!", async () => {
-    let listener = null;
+  xdescribe("Normal events", () => {
+    it("Single event works", async () => {
+      let listener = null;
 
-    let [event, slot] = await new Promise((resolve, _reject) => {
-      listener = program.addEventListener("MyEvent", (event, slot) => {
-        resolve([event, slot]);
+      let [event, slot] = await new Promise((resolve, _reject) => {
+        listener = program.addEventListener("MyEvent", (event, slot) => {
+          resolve([event, slot]);
+        });
+        program.rpc.initialize();
       });
-      program.rpc.initialize();
-    });
-    await program.removeEventListener(listener);
+      await program.removeEventListener(listener);
 
-    assert.isAbove(slot, 0);
-    assert.strictEqual(event.data.toNumber(), 5);
-    assert.strictEqual(event.label, "hello");
+      assert.isAbove(slot, 0);
+      assert.strictEqual(event.data.toNumber(), 5);
+      assert.strictEqual(event.label, "hello");
+    });
+
+    it("Multiple events work", async () => {
+      let listenerOne = null;
+      let listenerTwo = null;
+
+      let [eventOne, slotOne] = await new Promise((resolve, _reject) => {
+        listenerOne = program.addEventListener("MyEvent", (event, slot) => {
+          resolve([event, slot]);
+        });
+        program.rpc.initialize();
+      });
+
+      let [eventTwo, slotTwo] = await new Promise((resolve, _reject) => {
+        listenerTwo = program.addEventListener(
+          "MyOtherEvent",
+          (event, slot) => {
+            resolve([event, slot]);
+          }
+        );
+        program.rpc.testEvent();
+      });
+
+      await program.removeEventListener(listenerOne);
+      await program.removeEventListener(listenerTwo);
+
+      assert.isAbove(slotOne, 0);
+      assert.strictEqual(eventOne.data.toNumber(), 5);
+      assert.strictEqual(eventOne.label, "hello");
+
+      assert.isAbove(slotTwo, 0);
+      assert.strictEqual(eventTwo.data.toNumber(), 6);
+      assert.strictEqual(eventTwo.label, "bye");
+    });
   });
 
-  it("Multiple events", async () => {
-    // Sleep so we don't get this transaction has already been processed.
-    await sleep(2000);
+  describe("CPI events", () => {
+    it("Works without accounts being specified", async () => {
+      const tx = await program.methods.testEventCpi().transaction();
+      const config = {
+        commitment: "confirmed",
+      };
+      const txHash = await program.provider.sendAndConfirm(tx, [], config);
+      const txResult = await program.provider.connection.getTransaction(
+        txHash,
+        config
+      );
 
-    let listenerOne = null;
-    let listenerTwo = null;
+      const ixData = anchor.utils.bytes.bs58.decode(
+        txResult.meta.innerInstructions[0].instructions[0].data
+      );
+      const eventData = anchor.utils.bytes.base64.encode(ixData.slice(8));
+      const event = program.coder.events.decode(eventData);
 
-    let [eventOne, slotOne] = await new Promise((resolve, _reject) => {
-      listenerOne = program.addEventListener("MyEvent", (event, slot) => {
-        resolve([event, slot]);
-      });
-      program.rpc.initialize();
+      assert.strictEqual(event.name, "MyOtherEvent");
+      assert.strictEqual(event.data.label, "cpi");
+      assert.strictEqual(event.data.data.toNumber(), 7);
     });
 
-    let [eventTwo, slotTwo] = await new Promise((resolve, _reject) => {
-      listenerTwo = program.addEventListener("MyOtherEvent", (event, slot) => {
-        resolve([event, slot]);
-      });
-      program.rpc.testEvent();
+    it("Malicious invocation throws", async () => {
+      const tx = new anchor.web3.Transaction();
+      tx.add(
+        new anchor.web3.TransactionInstruction({
+          programId: program.programId,
+          keys: [
+            {
+              pubkey: anchor.web3.PublicKey.findProgramAddressSync(
+                [Buffer.from("__event_authority")],
+                program.programId
+              )[0],
+              isSigner: false,
+              isWritable: false,
+            },
+            {
+              pubkey: program.programId,
+              isSigner: false,
+              isWritable: false,
+            },
+          ],
+          data: Buffer.from([0xe4, 0x45, 0xa5, 0x2e, 0x51, 0xcb, 0x9a, 0x1d]),
+        })
+      );
+
+      try {
+        await program.provider.sendAndConfirm(tx, []);
+      } catch {
+        return;
+      }
+
+      throw new Error("Was able to invoke the self-CPI instruction");
     });
-
-    await program.removeEventListener(listenerOne);
-    await program.removeEventListener(listenerTwo);
-
-    assert.isAbove(slotOne, 0);
-    assert.strictEqual(eventOne.data.toNumber(), 5);
-    assert.strictEqual(eventOne.label, "hello");
-
-    assert.isAbove(slotTwo, 0);
-    assert.strictEqual(eventTwo.data.toNumber(), 6);
-    assert.strictEqual(eventTwo.label, "bye");
-  });
-
-  it("Self-CPI events work", async () => {
-    await sleep(200);
-
-    const tx = await program.methods.testEventCpi().transaction();
-    const config = {
-      commitment: "confirmed",
-    };
-    const txHash = await program.provider.sendAndConfirm(tx, [], config);
-    const txResult = await program.provider.connection.getTransaction(
-      txHash,
-      config
-    );
-
-    const ixData = anchor.utils.bytes.bs58.decode(
-      txResult.meta.innerInstructions[0].instructions[0].data
-    );
-    const eventData = anchor.utils.bytes.base64.encode(ixData.slice(8));
-    const event = program.coder.events.decode(eventData);
-
-    assert.strictEqual(event.name, "MyOtherEvent");
-    assert.strictEqual(event.data.label, "cpi");
-    assert.strictEqual(event.data.data.toNumber(), 7);
   });
 });
-
-function sleep(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
