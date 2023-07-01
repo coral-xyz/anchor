e55cd3e646c139d9045a752900b5294dc0f6c695
acheron <98934430+acheroncrypto@users.noreply.github.com>
2023-07-01 22:59:36 +0200

    lang: Add `Lamports` trait (#2552)

---
+++ tests/misc/tests/lamports/lamports.ts
@@ -0,0 +1,23 @@
+import * as anchor from "@coral-xyz/anchor";
+
+import { Lamports, IDL } from "../../target/types/lamports";
+
+describe(IDL.name, () => {
+  // Configure the client to use the local cluster
+  anchor.setProvider(anchor.AnchorProvider.env());
+
+  const program = anchor.workspace.Lamports as anchor.Program<Lamports>;
+
+  it("Can use the Lamports trait", async () => {
+    const signer = program.provider.publicKey!;
+    const [pda] = anchor.web3.PublicKey.findProgramAddressSync(
+      [Buffer.from("lamports")],
+      program.programId
+    );
+
+    await program.methods
+      .testLamportsTrait(new anchor.BN(anchor.web3.LAMPORTS_PER_SOL))
+      .accounts({ signer, pda })
+      .rpc();
+  });
+});
