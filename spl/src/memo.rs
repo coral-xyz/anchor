8ed16a5b330bde39d5c47342affe61c3400914c8
0xWoo <keisukew53@gmail.com>
2023-10-12 13:02:25 -0400

    chore: resolve feedback

--- spl/src/memo.rs
+++ spl/src/memo.rs
@@ -6,12 +6,12 @@ pub use spl_memo;
 pub use spl_memo::ID;
 
 pub fn build_memo<'info>(ctx: CpiContext<'_, '_, '_, 'info, BuildMemo>, memo: &[u8]) -> Result<()> {
-    let mut signer_pubkeys: Vec<&Pubkey> = vec![];
-    for i in 0..ctx.remaining_accounts.len() {
-        signer_pubkeys.push(ctx.remaining_accounts[i].key);
-    }
-
-    let ix = spl_memo::build_memo(memo, &signer_pubkeys);
+    let ix = spl_memo::build_memo(
+        memo,
+        &ctx.remaining_accounts
+            .map(|account| account.key)
+            .collect::<Vec<_>>(),
+    );
     solana_program::program::invoke_signed(&ix, &ctx.remaining_accounts, ctx.signer_seeds)
         .map_err(Into::into)
 }
