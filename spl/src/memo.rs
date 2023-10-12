9076df8a6e9806196a6382c99e6cf8b60d2217e1
0xWoo <keisukew53@gmail.com>
2023-10-12 00:09:19 -0400

    spl: add feature memo to support cpi to spl-memo

---
+++ spl/src/memo.rs
@@ -0,0 +1,33 @@
+use anchor_lang::prelude::{CpiContext, Signer};
+use anchor_lang::solana_program::pubkey::Pubkey;
+use anchor_lang::{Accounts, Result, ToAccountInfo};
+
+pub use spl_memo;
+pub use spl_memo::ID;
+
+pub fn build_memo<'info>(
+    ctx: CpiContext<'_, '_, '_, 'info, BuildMemo<'info>>,
+    memo: &[u8],
+) -> Result<()> {
+    let ix = spl_memo::build_memo(memo, &[ctx.accounts.signer.key]);
+    solana_program::program::invoke_signed(
+        &ix,
+        &[ctx.accounts.signer.to_account_info()],
+        ctx.signer_seeds,
+    )
+    .map_err(Into::into)
+}
+
+#[derive(Accounts)]
+pub struct BuildMemo<'info> {
+    pub signer: Signer<'info>,
+}
+
+#[derive(Clone)]
+pub struct Memo;
+
+impl anchor_lang::Id for Memo {
+    fn id() -> Pubkey {
+        ID
+    }
+}
