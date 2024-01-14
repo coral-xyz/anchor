95bfc906564d2dea15eebff5e4c42c42c8d54025
toma <yiyiaxpb@gmail.com>
2024-01-14 14:50:54 +0900

    change: testとか動くようにした

--- examples/tutorial/basic-0/programs/basic-0/src/lib.rs
+++ examples/tutorial/basic-0/programs/basic-0/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("DzVuV6qMC2oJJEwerpYrenPDhTQuHQRfMe4LdCmMZJYK");
 
 #[program]
 mod basic_0 {
