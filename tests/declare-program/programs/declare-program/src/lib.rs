1c153b19c39f2d9e5bfa8efb583f5b59aa8f627a
acheron <acheroncrypto@gmail.com>
2024-03-25 21:40:42 +0100

    Add tests

---
+++ tests/declare-program/programs/declare-program/src/lib.rs
@@ -0,0 +1,39 @@
+use anchor_lang::prelude::*;
+
+declare_id!("Dec1areProgram11111111111111111111111111111");
+
+declare_program!(external);
+use external::program::External;
+
+#[program]
+pub mod declare_program {
+    use super::*;
+
+    pub fn cpi(ctx: Context<Cpi>, value: u32) -> Result<()> {
+        let cpi_my_account = &mut ctx.accounts.cpi_my_account;
+        require_keys_eq!(external::accounts::MyAccount::owner(), external::ID);
+        require_eq!(cpi_my_account.field, 0);
+
+        let cpi_ctx = CpiContext::new(
+            ctx.accounts.external_program.to_account_info(),
+            external::cpi::accounts::Update {
+                authority: ctx.accounts.authority.to_account_info(),
+                my_account: cpi_my_account.to_account_info(),
+            },
+        );
+        external::cpi::update(cpi_ctx, value)?;
+
+        cpi_my_account.reload()?;
+        require_eq!(cpi_my_account.field, value);
+
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Cpi<'info> {
+    pub authority: Signer<'info>,
+    #[account(mut)]
+    pub cpi_my_account: Account<'info, external::accounts::MyAccount>,
+    pub external_program: Program<'info, External>,
+}
---
+++ tests/declare-program/programs/external/src/lib.rs
@@ -0,0 +1,44 @@
+use anchor_lang::prelude::*;
+
+declare_id!("Externa111111111111111111111111111111111111");
+
+#[program]
+pub mod external {
+    use super::*;
+
+    pub fn init(_ctx: Context<Init>) -> Result<()> {
+        Ok(())
+    }
+
+    pub fn update(ctx: Context<Update>, value: u32) -> Result<()> {
+        ctx.accounts.my_account.field = value;
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Init<'info> {
+    #[account(mut)]
+    pub authority: Signer<'info>,
+    #[account(
+        init,
+        payer = authority,
+        space = 8 + 4,
+        seeds = [authority.key.as_ref()],
+        bump
+    )]
+    pub my_account: Account<'info, MyAccount>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Update<'info> {
+    pub authority: Signer<'info>,
+    #[account(mut, seeds = [authority.key.as_ref()], bump)]
+    pub my_account: Account<'info, MyAccount>,
+}
+
+#[account]
+pub struct MyAccount {
+    pub field: u32,
+}
