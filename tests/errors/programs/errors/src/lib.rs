8babd8727007a551238f31b6dd21641753b69a13
CanardMandarin <thibault.marboud@gmail.com>
2024-05-10 07:08:51 +0000

    add tests

--- tests/errors/programs/errors/src/lib.rs
+++ tests/errors/programs/errors/src/lib.rs
@@ -131,6 +131,10 @@ mod errors {
         require_gte!(5, 10);
         Ok(())
     }
+
+    pub fn try_into_integer(_ctx: Context<Hello>) -> Result<i64> {
+        Ok(u64::MAX.try_into()?)
+    }
 }
 
 #[derive(Accounts)]
