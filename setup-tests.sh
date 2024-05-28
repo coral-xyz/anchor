2ee75c25971adbdec58e249bcf44ff3741dd347e
Loris Leiva <loris.leiva@gmail.com>
2024-05-28 13:25:24 +0100

    Add anchor-errors package to CI and setup scripts

--- setup-tests.sh
+++ setup-tests.sh
@@ -7,6 +7,7 @@ fi
 
 git submodule update --init --recursive --depth 1
 cd ts/packages/borsh && yarn --frozen-lockfile && yarn build && yarn link --force && cd ../../../
+cd ts/packages/anchor-errors && yarn --frozen-lockfile && yarn build && yarn link --force && cd ../../../
 cd ts/packages/anchor && yarn --frozen-lockfile && yarn build:node && yarn link && cd ../../../
 cd ts/packages/spl-associated-token-account && yarn --frozen-lockfile && yarn build:node && yarn link && cd ../../../
 cd ts/packages/spl-token && yarn --frozen-lockfile && yarn build:node && yarn link && cd ../../../
