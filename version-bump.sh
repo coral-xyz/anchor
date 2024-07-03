4f5ac74296189abea82d7f4a6f182f98b6645a1d
acheron <acheroncrypto@gmail.com>
2024-07-03 17:31:01 +0200

    Fix false positives when using `version-bump.sh` script

--- version-bump.sh
+++ version-bump.sh
@@ -18,25 +18,23 @@ case "$(uname)" in
   Darwin*) sedi=(-i "")
 esac
 
-# Don't replace version with the following globs
-skip_globs=":!**/yarn.lock :!Cargo.lock :!package.json :!tests/bench/bench.json :!bench/*.md"
-
-git grep -l $(cat VERSION) -- $skip_globs |
+# Only replace version with the following globs
+allow_globs=":**/Cargo.toml **/Makefile docs/src/pages/docs/*.md client/src/lib.rs"
+git grep -l $(cat VERSION) -- $allow_globs |
     xargs sed "${sedi[@]}" \
     -e "s/$(cat VERSION)/$version/g"
 
-# Potential for collisions in package.json files, handle those separately
+# Potential for collisions in `package.json` files, handle those separately
 # Replace only matching "version": "x.xx.x" and "@coral-xyz/anchor": "x.xx.x"
-git grep -l $(cat VERSION) -- '**/package.json' | \
+git grep -l $(cat VERSION) -- "**/package.json" | \
     xargs sed "${sedi[@]}" \
     -e "s/@coral-xyz\/anchor\": \"$(cat VERSION)\"/@coral-xyz\/anchor\": \"$version\"/g" \
     -e "s/\"version\": \"$(cat VERSION)\"/\"version\": \"$version\"/g"
 
-# Potential for collisions in Cargo.lock, use cargo update to update it
-cargo update --workspace
-
-# Insert version number into CHANGELOG.md
-sed "${sedi[@]}" -e "s/## \[Unreleased\]/## [Unreleased]\n\n## [$version] - $(date '+%Y-%m-%d')/g" CHANGELOG.md
+# Insert version number into CHANGELOG
+sed "${sedi[@]}" -e \
+    "s/## \[Unreleased\]/## [Unreleased]\n\n### Features\n\n### Fixes\n\n### Breaking\n\n## [$version] - $(date '+%Y-%m-%d')/g" \
+    CHANGELOG.md
 
 pushd ts && yarn && popd
 pushd tests && yarn && popd
