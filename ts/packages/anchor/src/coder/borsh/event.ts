14fcdf491227c15b1fd28eda56cf349bc2594a11
acheron <acheroncrypto@gmail.com>
2024-07-26 15:20:48 +0200

    Get event discriminator lengths dynamically

--- ts/packages/anchor/src/coder/borsh/event.ts
+++ ts/packages/anchor/src/coder/borsh/event.ts
@@ -1,7 +1,7 @@
 import { Buffer } from "buffer";
 import { Layout } from "buffer-layout";
 import * as base64 from "../../utils/bytes/base64.js";
-import { Idl } from "../../idl.js";
+import { Idl, IdlDiscriminator } from "../../idl.js";
 import { IdlCoder } from "./idl.js";
 import { EventCoder } from "../index.js";
 
@@ -9,12 +9,10 @@ export class BorshEventCoder implements EventCoder {
   /**
    * Maps account type identifier to a layout.
    */
-  private layouts: Map<string, Layout>;
-
-  /**
-   * Maps base64 encoded event discriminator to event name.
-   */
-  private discriminators: Map<string, string>;
+  private layouts: Map<
+    string,
+    { discriminator: IdlDiscriminator; layout: Layout }
+  >;
 
   public constructor(idl: Idl) {
     if (!idl.events) {
@@ -27,21 +25,20 @@ export class BorshEventCoder implements EventCoder {
       throw new Error("Events require `idl.types`");
     }
 
-    const layouts: [string, Layout<any>][] = idl.events.map((ev) => {
+    const layouts = idl.events.map((ev) => {
       const typeDef = types.find((ty) => ty.name === ev.name);
       if (!typeDef) {
         throw new Error(`Event not found: ${ev.name}`);
       }
-      return [ev.name, IdlCoder.typeDefLayout({ typeDef, types })];
+      return [
+        ev.name,
+        {
+          discriminator: ev.discriminator,
+          layout: IdlCoder.typeDefLayout({ typeDef, types }),
+        },
+      ] as const;
     });
     this.layouts = new Map(layouts);
-
-    this.discriminators = new Map<string, string>(
-      (idl.events ?? []).map((ev) => [
-        base64.encode(Buffer.from(ev.discriminator)),
-        ev.name,
-      ])
-    );
   }
 
   public decode(log: string): {
@@ -55,19 +52,18 @@ export class BorshEventCoder implements EventCoder {
     } catch (e) {
       return null;
     }
-    const disc = base64.encode(logArr.slice(0, 8));
 
-    // Only deserialize if the discriminator implies a proper event.
-    const eventName = this.discriminators.get(disc);
-    if (!eventName) {
-      return null;
+    for (const [name, layout] of this.layouts) {
+      const givenDisc = logArr.subarray(0, layout.discriminator.length);
+      const matches = givenDisc.equals(Buffer.from(layout.discriminator));
+      if (matches) {
+        return {
+          name,
+          data: layout.layout.decode(logArr.subarray(givenDisc.length)),
+        };
+      }
     }
 
-    const layout = this.layouts.get(eventName);
-    if (!layout) {
-      throw new Error(`Unknown event: ${eventName}`);
-    }
-    const data = layout.decode(logArr.slice(8));
-    return { data, name: eventName };
+    return null;
   }
 }
