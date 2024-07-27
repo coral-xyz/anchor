d38b143cf4fb19d489fac5dc54ad94ba101af060
Bhargava Sai Macha <mbsai29@gmail.com>
2024-07-27 16:04:54 -0400

    fix function args

--- lang/syn/src/codegen/accounts/constraints.rs
+++ lang/syn/src/codegen/accounts/constraints.rs
@@ -860,15 +860,15 @@ fn generate_constraint_init_group(
             };
 
             let interest_bearing_mint_rate = match interest_bearing_mint_rate {
-                Some(ibmr) => quote! { Option::<&i16>::Some(&#ibmr) },
-                None => quote! { Option::<&anchor_lang::prelude::Pubkey>::None },
+                Some(ibmr) => quote! { Option::<i16>::Some(#ibmr) },
+                None => quote! { Option::<i16>::None },
             };
 
             let interest_bearing_mint_authority = match interest_bearing_mint_authority {
                 Some(ibma) => {
-                    quote! { Option::<&anchor_lang::prelude::Pubkey>::Some(&#ibma.key()) }
+                    quote! { Option::<anchor_lang::prelude::Pubkey>::Some(#ibma.key()) }
                 }
-                None => quote! { Option::<&anchor_lang::prelude::Pubkey>::None },
+                None => quote! { Option::<anchor_lang::prelude::Pubkey>::None },
             };
 
             let transfer_hook_authority = match transfer_hook_authority {
@@ -957,7 +957,7 @@ fn generate_constraint_init_group(
                                         ::anchor_spl::token_interface::interest_bearing_mint_initialize(anchor_lang::context::CpiContext::new(#token_program.to_account_info(), ::anchor_spl::token_interface::InterestBearingMintInitialize {
                                             token_program_id: #token_program.to_account_info(),
                                             mint: #field.to_account_info(),
-                                        }), #interest_bearing_mint_authority.unwrap(), #interest_bearing_mint_rate.unwrap())?;
+                                        }), #interest_bearing_mint_authority, #interest_bearing_mint_rate.unwrap())?;
                                     },
                                     // All extensions specified by the user should be implemented.
                                     // If this line runs, it means there is a bug in the codegen.
