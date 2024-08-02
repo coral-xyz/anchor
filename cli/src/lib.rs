8f3175ffd5c3f81a67f7a65e5dce9a62478cded7
acheron <acheroncrypto@gmail.com>
2024-08-02 23:13:33 +0200

    Fix `clippy::needless_borrows_for_generic_args`

--- cli/src/lib.rs
+++ cli/src/lib.rs
@@ -3811,7 +3811,7 @@ fn upgrade(
             .arg("--url")
             .arg(url)
             .arg("--keypair")
-            .arg(&cfg.provider.wallet.to_string())
+            .arg(cfg.provider.wallet.to_string())
             .arg("--program-id")
             .arg(strip_workspace_prefix(program_id.to_string()))
             .arg(strip_workspace_prefix(program_filepath))
