8e9f1e7a2b2aeea5756be9d4ea757168a1d60ba8
David Leng <davidleng0x0@foxmail.com>
2024-08-31 05:40:53 +0800

    cli: Fix template code shouldn't escape (#3210)

--- cli/src/rust_template.rs
+++ cli/src/rust_template.rs
@@ -143,7 +143,7 @@ pub use initialize::*;
 pub struct Initialize {}
 
 pub fn handler(ctx: Context<Initialize>) -> Result<()> {
-    msg!("Greetings from: {{:?}}", ctx.program_id);
+    msg!("Greetings from: {:?}", ctx.program_id);
     Ok(())
 }
 "#
