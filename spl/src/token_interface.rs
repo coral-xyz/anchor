24184aa0eddc8e8d34886b45eaab11d4fcdc67ae
acheron <acheroncrypto@gmail.com>
2024-09-02 17:26:06 +0200

    spl: Fix missing `Pod` trait import
    `Pod` trait is no longer exported from `spl-token-2022`, use the export from `anchor-lang`

--- spl/src/token_interface.rs
+++ spl/src/token_interface.rs
@@ -1,10 +1,8 @@
+use anchor_lang::__private::bytemuck::Pod;
 use anchor_lang::solana_program::program_pack::Pack;
 use anchor_lang::solana_program::pubkey::Pubkey;
 use spl_token_2022::extension::ExtensionType;
-use spl_token_2022::{
-    extension::{BaseStateWithExtensions, Extension, StateWithExtensions},
-    solana_zk_token_sdk::instruction::Pod,
-};
+use spl_token_2022::extension::{BaseStateWithExtensions, Extension, StateWithExtensions};
 use std::ops::Deref;
 
 pub use crate::token_2022::*;
