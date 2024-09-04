6ce06f8df26054adad43082159a208db38246ffa
acheron <acheroncrypto@gmail.com>
2024-09-04 17:11:46 +0200

    Remove `state` implementation from SPL custom coders

--- ts/packages/spl-name-service/src/coder/index.ts
+++ ts/packages/spl-name-service/src/coder/index.ts
@@ -3,7 +3,6 @@ import { Idl, Coder } from "@coral-xyz/anchor";
 import { SplNameServiceAccountsCoder } from "./accounts";
 import { SplNameServiceEventsCoder } from "./events";
 import { SplNameServiceInstructionCoder } from "./instructions";
-import { SplNameServiceStateCoder } from "./state";
 import { SplNameServiceTypesCoder } from "./types";
 
 /**
@@ -13,14 +12,12 @@ export class SplNameServiceCoder implements Coder {
   readonly accounts: SplNameServiceAccountsCoder;
   readonly events: SplNameServiceEventsCoder;
   readonly instruction: SplNameServiceInstructionCoder;
-  readonly state: SplNameServiceStateCoder;
   readonly types: SplNameServiceTypesCoder;
 
   constructor(idl: Idl) {
     this.accounts = new SplNameServiceAccountsCoder(idl);
     this.events = new SplNameServiceEventsCoder(idl);
     this.instruction = new SplNameServiceInstructionCoder(idl);
-    this.state = new SplNameServiceStateCoder(idl);
     this.types = new SplNameServiceTypesCoder(idl);
   }
 }
