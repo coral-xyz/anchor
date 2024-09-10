8a634c757b231f9e6940952ec5c95f73f7659a62
Yash Goyal <yashgo0018@gmail.com>
2024-09-11 03:26:30 +0530

    cli: Use ESM imports and annote provider type in the default migration script (#3234)
    Co-authored-by: acheron <98934430+acheroncrypto@users.noreply.github.com>

--- cli/src/rust_template.rs
+++ cli/src/rust_template.rs
@@ -303,9 +303,9 @@ pub fn ts_deploy_script() -> &'static str {
 // single deploy script that's invoked from the CLI, injecting a provider
 // configured from the workspace's Anchor.toml.
 
-const anchor = require("@coral-xyz/anchor");
+import * as anchor from "@coral-xyz/anchor";
 
-module.exports = async function (provider) {
+module.exports = async function (provider: anchor.AnchorProvider) {
   // Configure client to use the provider.
   anchor.setProvider(provider);
 
