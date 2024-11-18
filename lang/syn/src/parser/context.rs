459314eb7910fcd2499db77141842296d007ea56
Zhang ShengYan <yan.code@gmail.com>
2024-11-18 10:01:43 +0800

    feat:introduce skip-lint in safety_checks error

--- lang/syn/src/parser/context.rs
+++ lang/syn/src/parser/context.rs
@@ -70,6 +70,8 @@ impl CrateContext {
         {}:{}:{}
         Struct field "{}" is unsafe, but is not documented.
         Please add a `/// CHECK:` doc comment explaining why no checks through types are necessary.
+        Alternatively, for reasons like quick prototyping, you may disable the safety checks
+        by using the `skip-lint` option.
         See https://www.anchor-lang.com/docs/the-accounts-struct#safety-checks for more information.
                     "#,
                         ctx.file.canonicalize().unwrap().display(),
