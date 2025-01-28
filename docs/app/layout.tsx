82660b2476532412619b0ea8542f5d28808d5cf4
John <johnzhengyuliu@gmail.com>
2025-01-28 14:43:37 -0600

    add google analytics id

--- docs/app/layout.tsx
+++ docs/app/layout.tsx
@@ -13,7 +13,7 @@ export default function Layout({ children }: { children: ReactNode }) {
       <body className="flex flex-col min-h-screen">
         <RootProvider>{children}</RootProvider>
       </body>
-      <GoogleAnalytics gaId="" />
+      <GoogleAnalytics gaId="G-ZJYNM2WNM0" />
     </html>
   );
 }
